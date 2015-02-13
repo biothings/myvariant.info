3e7f92a89ac2764c651bab94e58646ea6a98aa1f
amarkscripps <Amark@dhcp-58-111.scripps.edu>
2015-02-13 09:48:56 -0800

    new dbnsfp

--- src/dataload/contrib/cadd/__init__.py
+++ src/dataload/contrib/cadd/__init__.py
@@ -2,8 +2,8 @@
 
 from .cadd_parser import load_data as _load_data
 
-
-CADD_INPUT_FILE = 'http://krishna.gs.washington.edu/download/CADD/v1.0/whole_genome_SNVs_inclAnno.tsv.gz'
+#CADD_INPUT = 'http://krishna.gs.washington.edu/download/CADD/v1.2/whole_genome_SNVs.tsv.gz'
+CADD_INPUT_ALL = 'http://krishna.gs.washington.edu/download/CADD/v1.1/whole_genome_SNVs_inclAnno.tsv.gz'
 
 
 def load_data():
--- src/dataload/contrib/cadd/cadd_parser.py
+++ src/dataload/contrib/cadd/cadd_parser.py
@@ -11,200 +11,200 @@ DEPENDENCIES = ["pysam", "pymongo"]
 
 # convert one snp to json
 def _map_line_to_json(fields):
-    if len(fields) >= VALID_COLUMN_NO:
-        chrom = fields[0]
-        chromStart = fields[1]
-        allele1 = fields[2]
-        allele2 = fields[4]
-        HGVS = "chr%s:g.%s%s>%s" % (chrom, chromStart, allele1, allele2)
-    
-        # load as json data
-        if HGVS is None:
-            return
-    
-        one_snp_json = {
-    
-                "_id": HGVS,
-                "cadd":
-                    {
-                         'chrom': fields[0],
-                         'pos': fields[1],
-                         'ref': fields[2],
-                         'anc': fields[3],
-                         'alt': fields[4],
-                         'type': fields[5],
-                         'length': fields[6],
-                         'istv': fields[7],
-                         'isderived': fields[8],
-                         'annotype': fields[9],
-                         'consequence': fields[10],
-                         'consscore': fields[11],
-                         'consdetail': fields[12],
-                         'gc': fields[13],
-                         'cpg': fields[14],
-                         'mapability':
-                             {
-                                 '20bp': fields[15],
-                                 '35bp': fields[16]
-                             },
-                         'scoresegdup': fields[17],
-                         'phast_cons':
-                             {
-                                 'primate': fields[18],
-                                 'mammalian': fields[19],
-                                 'vertebrate': fields[20]
-                             },
-                         'phylop':
-                             {
-                                 'primate': fields[21],
-                                 'mammalian': fields[22],
-                                 'vertebrate': fields[23]
-                             },
-                         'gerp':
-                             {
-                                 'n': fields[24],
-                                 's': fields[25],
-                                 'rs': fields[26],
-                                 'rs_pval': fields[27]
-                             },
-                         'bstatistic': fields[28],
-                         'mutindex': fields[29],
-                         'dna':
-                             {
-                                 'helt': fields[30],
-                                 'mgw': fields[31],
-                                 'prot': fields[32],
-                                 'roll': fields[33]
-                             },
-                         'mirsvr':
-                             {
-                                'score': fields[34],
-                                'e': fields[35],
-                                'aln': fields[36]
-                             },
-                         'targetscans': fields[37],
-                         'fitcons': fields[38],
-                         'chmm':
-                             {
-                                'tssa': fields[39],
-                                'tssaflnk': fields[40],
-                                'txflnk': fields[41],
-                                'tx': fields[42],
-                                'txwk': fields[43],
-                                'enh': fields[44],
-                                #'enh': fields[45],
-                                'znfrpts': fields[46],
-                                'het': fields[47],
-                                'tssbiv': fields[48],
-                                'bivflnk': fields[49],
-                                'enhbiv': fields[50],
-                                'reprpc': fields[51],
-                                'reprpcwk': fields[52],
-                                'quies': fields[53],
-                             },
-                         'encode':
-                             {
-                                 'exp': fields[54],
-                                 'h3k27ac': fields[55],
-                                 'h3k4me1': fields[56],
-                                 'h3k4me3': fields[57],
-                                 'nucleo': fields[58],
-                                 'occ': fields[59],
-                                 'p_val':
-                                     {
-                                         'comb': fields[60],
-                                         'dnas': fields[61],
-                                         'faire': fields[62],
-                                         'polii': fields[63],
-                                         'ctcf': fields[64],
-                                         'mycp': fields[65]
-                                     },
-                                 'sig':
-                                     {
-                                         'dnase': fields[66],
-                                         'faire': fields[67],
-                                         'polii': fields[68],
-                                         'ctcf': fields[69],
-                                         'myc': fields[70]
-                                     },
-                             },
-                         'segway': fields[71],
-                         'motif':
-                             {
-                                 'toverlap': fields[72],
-                                 'dist': fields[73],
-                                 'ecount': fields[74],
-                                 'ename': fields[75],
-                                 'ehipos': fields[76],
-                                 'escorechng': fields[77]
-                             },
-                         'tf':
-                             {
-                                 'bs': fields[78],
-                                 'bs_peaks': fields[79],
-                                 'bs_peaks_max': fields[80]
-                             },
-                         'isknownvariant': fields[81],
-                         'esp':
-                             {
-                                 'af': fields[82],
-                                 'afr': fields[83],
-                                 'eur': fields[84]
-                             },
-                         '1000g':
-                             {
-                                 'af': fields[85],
-                                 'asn': fields[86],
-                                 'amr': fields[87],
-                                 'afr': fields[88],
-                                 'eur': fields[89]
-                             },
-                         'min_dist_tss': fields[90],
-                         'min_dist_tse': fields[91],
-                         'gene':
-                             {
-                                 'gene_id': fields[92],
-                                 'feature_id': fields[93],
-                                 'ccds_id': fields[94],
-                                 'genename': fields[95],
-                                 'cds':
-                                     {
-                                         'cdna_pos': fields[96],
-                                         'rel_cdna_pos': fields[97],
-                                         'cds_pos': fields[98],
-                                         'rel_cds_pos': fields[99]
-                                     },
-                                 'prot':
-                                     {
-                                         'protpos': fields[100],
-                                         'rel_prot_pos': fields[101],
-                                         'domain': fields[102]
-                                     }
-                                },
-                         'dst2splice': fields[103],
-                         'dst2spltype': fields[104],
-                         'exon': fields[105],
-                         'intron': fields[106],
-                         'oaa': fields[107],
-                         'naa': fields[108],
-                            
-                         'grantham': fields[109],
-                         'polyphen':
-                             {
-                                 'cat': fields[110],
-                                 'val': fields[111]
-                             },
-                         'sift':
-                             {
-                                 'cat': fields[112],
-                                 'val': fields[113]
-                             },
-                         'rawscore': fields[114],
-                         'phred': fields[115]
-                      }
-                }
+    #if len(fields) >= VALID_COLUMN_NO:
+    chrom = fields[0]
+    chromStart = fields[1]
+    allele1 = fields[2]
+    allele2 = fields[4]
+    HGVS = "chr%s:g.%s%s>%s" % (chrom, chromStart, allele1, allele2)
 
-        return dict_sweep(unlist(value_convert(one_snp_json)), ["NA"])
+    # load as json data
+    if HGVS is None:
+        return
+
+    one_snp_json = {
+
+            "_id": HGVS,
+            "cadd":
+                {
+                     'chrom': fields[0],
+                     'pos': fields[1],
+                     'ref': fields[2],
+                     'anc': fields[3],
+                     'alt': fields[4],
+                     'type': fields[5],
+                     'length': fields[6],
+                     'istv': fields[7],
+                     'isderived': fields[8],
+                     'annotype': fields[9],
+                     'consequence': fields[10],
+                     'consscore': fields[11],
+                     'consdetail': fields[12],
+                     'gc': fields[13],
+                     'cpg': fields[14],
+                     'mapability':
+                         {
+                             '20bp': fields[15],
+                             '35bp': fields[16]
+                         },
+                     'scoresegdup': fields[17],
+                     'phast_cons':
+                         {
+                             'primate': fields[18],
+                             'mammalian': fields[19],
+                             'vertebrate': fields[20]
+                         },
+                     'phylop':
+                         {
+                             'primate': fields[21],
+                             'mammalian': fields[22],
+                             'vertebrate': fields[23]
+                         },
+                     'gerp':
+                         {
+                             'n': fields[24],
+                             's': fields[25],
+                             'rs': fields[26],
+                             'rs_pval': fields[27]
+                         },
+                     'bstatistic': fields[28],
+                     'mutindex': fields[29],
+                     'dna':
+                         {
+                             'helt': fields[30],
+                             'mgw': fields[31],
+                             'prot': fields[32],
+                             'roll': fields[33]
+                         },
+                     'mirsvr':
+                         {
+                            'score': fields[34],
+                            'e': fields[35],
+                            'aln': fields[36]
+                         },
+                     'targetscans': fields[37],
+                     'fitcons': fields[38],
+                     'chmm':
+                         {
+                            'tssa': fields[39],
+                            'tssaflnk': fields[40],
+                            'txflnk': fields[41],
+                            'tx': fields[42],
+                            'txwk': fields[43],
+                            'enh': fields[44],
+                            #'enh': fields[45],
+                            'znfrpts': fields[46],
+                            'het': fields[47],
+                            'tssbiv': fields[48],
+                            'bivflnk': fields[49],
+                            'enhbiv': fields[50],
+                            'reprpc': fields[51],
+                            'reprpcwk': fields[52],
+                            'quies': fields[53],
+                         },
+                     'encode':
+                         {
+                             'exp': fields[54],
+                             'h3k27ac': fields[55],
+                             'h3k4me1': fields[56],
+                             'h3k4me3': fields[57],
+                             'nucleo': fields[58],
+                             'occ': fields[59],
+                             'p_val':
+                                 {
+                                     'comb': fields[60],
+                                     'dnas': fields[61],
+                                     'faire': fields[62],
+                                     'polii': fields[63],
+                                     'ctcf': fields[64],
+                                     'mycp': fields[65]
+                                 },
+                             'sig':
+                                 {
+                                     'dnase': fields[66],
+                                     'faire': fields[67],
+                                     'polii': fields[68],
+                                     'ctcf': fields[69],
+                                     'myc': fields[70]
+                                 },
+                         },
+                     'segway': fields[71],
+                     'motif':
+                         {
+                             'toverlap': fields[72],
+                             'dist': fields[73],
+                             'ecount': fields[74],
+                             'ename': fields[75],
+                             'ehipos': fields[76],
+                             'escorechng': fields[77]
+                         },
+                     'tf':
+                         {
+                             'bs': fields[78],
+                             'bs_peaks': fields[79],
+                             'bs_peaks_max': fields[80]
+                         },
+                     'isknownvariant': fields[81],
+                     'esp':
+                         {
+                             'af': fields[82],
+                             'afr': fields[83],
+                             'eur': fields[84]
+                         },
+                     '1000g':
+                         {
+                             'af': fields[85],
+                             'asn': fields[86],
+                             'amr': fields[87],
+                             'afr': fields[88],
+                             'eur': fields[89]
+                         },
+                     'min_dist_tss': fields[90],
+                     'min_dist_tse': fields[91],
+                     'gene':
+                         {
+                             'gene_id': fields[92],
+                             'feature_id': fields[93],
+                             'ccds_id': fields[94],
+                             'genename': fields[95],
+                             'cds':
+                                 {
+                                     'cdna_pos': fields[96],
+                                     'rel_cdna_pos': fields[97],
+                                     'cds_pos': fields[98],
+                                     'rel_cds_pos': fields[99]
+                                 },
+                             'prot':
+                                 {
+                                     'protpos': fields[100],
+                                     'rel_prot_pos': fields[101],
+                                     'domain': fields[102]
+                                 }
+                            },
+                     'dst2splice': fields[103],
+                     'dst2spltype': fields[104],
+                     'exon': fields[105],
+                     'intron': fields[106],
+                     'oaa': fields[107],
+                     'naa': fields[108],
+                        
+                     'grantham': fields[109],
+                     'polyphen':
+                         {
+                             'cat': fields[110],
+                             'val': fields[111]
+                         },
+                     'sift':
+                         {
+                             'cat': fields[112],
+                             'val': fields[113]
+                         },
+                     'rawscore': fields[114],
+                     'phred': fields[115]
+                  }
+            }
+
+    return dict_sweep(unlist(value_convert(one_snp_json)), ["NA"])
 
         
 ## replace None indices with ''
