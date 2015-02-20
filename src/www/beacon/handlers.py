78d44ed5de48a9f594734fb56022361ff79a4384
Chunlei Wu <anewgene@yahoo.com>
2015-02-19 21:10:22 -0800

    changes for new beacon spec.

--- src/www/beacon/handlers.py
+++ src/www/beacon/handlers.py
@@ -6,21 +6,28 @@ class WellderlyBeaconHandler(BaseHandler):
     esq = ESQuery()
 
     def get(self):
-        chr = self.get_argument('chr', None)
+        chrom = self.get_argument('chrom', None)
         pos = self.get_argument('pos', None)
         allele = self.get_argument('allele', None)
+        ref = self.get_argument('ref', None)
 
-        if chr and pos and allele:
+        if chrom and pos and allele:
             q = 'wellderly.chr:{} AND wellderly.pos:{} AND wellderly.alt:{}'
-            q = q.format(chr, pos, allele)
+            q = q.format(chrom, pos, allele)
+            if ref:
+                q += ' AND wellderly.ref:{}'.format(ref)
             res = self.esq.query(q, fields=None)
             if res and res.get('total') >= 1:
-                out = {'exist': True}
+                # out = {'exist': True}
+                out = 'YES'
             else:
-                out = {'exist': False}
+                # out = {'exist': False}
+                out = 'NO'
         else:
-            out = {'success': False, 'error': "Missing required parameters."}
-        self.return_json(out)
+            # out = {'success': False, 'error': "Missing required parameters."}
+            out = 'NULL'
+        #self.return_json(out)
+        self.write(out)
 
 
 APP_LIST = [
