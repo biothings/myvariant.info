c4a2b4e5c52d6648011dfc59edcc34aa5a30bb6d
kevinxin90 <kevinxin@scripps.edu>
2015-02-23 17:28:21 -0800

    validate collections from mongodb

--- src/utils/validate.py
+++ src/utils/validate.py
@@ -1,10 +1,17 @@
-from bitarray import bitarray
-import re
-import os
-import types
 
+
+from __future__ import print_function
+import re
+
+from bitarray import bitarray
+
+import utils.common
+import utils.mongo
+from config import HG19_DATAFILE
 
 # encode nucleotide into bit form
+
+
 def nuc_to_bit(sequence):
     code = {'A': bitarray('001'),
             'C': bitarray('010'),
@@ -21,44 +28,19 @@ def nuc_to_bit(sequence):
     seq_bit.encode(code, sequence)
     return(seq_bit)
 
+# encode bit form to nucleotide
 
-# load compressed chromsome fasta_to_bit file
 
-
-def loadobj(filename, mode='file'):
-    '''Loads a compressed object from disk file (or file-like handler) or
-        MongoDB gridfs file (mode='gridfs')
-           obj = loadobj('data.pyobj')
-
-           obj = loadobj(('data.pyobj', mongo_db), mode='gridfs')
-    '''
-    import gzip
-    import cPickle as pickle
-
-    if mode == 'gridfs':
-        import gridfs
-        filename, db = filename   # input is a tuple of (filename, mongo_db)
-        fs = gridfs.GridFS(db)
-        fobj = fs.get(filename)
-    else:
-        if type(filename) in types.StringTypes:
-            fobj = file(filename, 'rb')
-        else:
-            fobj = filename   # input is a file-like handler
-    gzfobj = gzip.GzipFile(fileobj=fobj)
-    try:
-        buffer = ""
-        while 1:
-            data = gzfobj.read()
-            if data == "":
-                break
-            buffer += data
-        object = pickle.loads(buffer)
-    finally:
-        gzfobj.close()
-        fobj.close()
-    return object
-
+def bit_to_nuc(sequence):
+    if sequence == bitarray('001'):
+        nuc = 'A'
+    elif sequence == bitarray('010'):
+        nuc = 'C'
+    elif sequence == bitarray('011'):
+        nuc = 'G'
+    elif sequence == bitarray('100'):
+        nuc = 'T'
+    return nuc
 
 '''parse variant name, print the variant name and
     return chromosome number, nucleotide position
@@ -69,7 +51,6 @@ def loadobj(filename, mode='file'):
 def parse(str):
     pat = 'chr(\w+):g\.(\d+)(\w)\>(\w)'
     mat = re.match(pat, str)
-    print str
     if mat:
         r = mat.groups()
         return (r[0], r[1], r[2])
@@ -77,37 +58,73 @@ def parse(str):
 
 class VariantValidator:
     def __init__(self):
-        self.data = 'NULL'
+        self.data = None
 
-    # validate single hgvs variant name
+    # validate single hgvs variant name  document
     def validate_hgvs(self, hgvs_id):
         r = parse(hgvs_id)
         if r:
             # get the chromosome sequence in bit form
-            if self.data == 'NULL':
-                global chr_dict1
-                chr_dict1 = loadobj('chr_dict.pyobj')
-                self.data = 1
+            if self.data is None:
+                self.data = utils.common.loadobj(HG19_DATAFILE)
             chr_bit = bitarray()
             if r[0] == 'M':
                 chr = 'MT'
             else:
                 chr = r[0]
-            chr_bit = chr_dict1[str(chr)]
+            chr_bit = self.data[str(chr)]
+
             # get the nucleotide in chromsome sequence in bit form
             nuc_chr_bit = bitarray()
             nuc_chr_bit = chr_bit[int(r[1])*3-3:int(r[1])*3]
-            # encode mutant nucleotide into bit form
-            mut_bit = bitarray()
-            mut_bit = nuc_to_bit(r[2])
-            if (nuc_chr_bit == mut_bit):
-                print 'TRUE\n'
-            else:
-                print 'FALSE\n'
+            nuc_chr = bit_to_nuc(nuc_chr_bit)
+
+            # compare HGVS id with genome
+            return r[2] == nuc_chr
         else:
-            print 'NULL\n'
+            return None
 
     # validate multiple hgvs variant name
-    def validate_many(self, *id_list):
-        for item in id_list:
-            self.validate_hgvs(item)
+    def validate_many(self, *args):
+        for item in args:
+            print(self.validate_hgvs(item))
+
+    def validate_src(self, collection, return_False=False,
+                     return_None=False, return_True=False):
+        return_dic = {False: return_False, True: return_True,
+                      None: return_None}
+
+        # read in the collection from mongodb
+        src = utils.mongo.get_src_db()
+        cursor = utils.mongo.doc_feeder(src[collection], step=10000)
+
+        print_only = not (return_False or return_None or return_True)
+        if not print_only:
+            # output dictionary, three keys: 'false','true','none'
+            out = {}
+            for k in return_dic:
+                if return_dic[k]:
+                    out[k] = []
+
+        # initialize the count
+        cnt_d = {True: 0, False: 0, None: 0}    # cnt_d
+        # validate each item in the cursor
+        for item in cursor:
+            _id = item['_id']
+            valid = self.validate_hgvs(_id)
+            print(_id, valid)
+            cnt_d[valid] += 1
+            if return_dic[valid]:
+                out[valid].append(_id)
+
+        # print out counts
+        print("The number of False HGVS IDs in the collection \
+is : {0}".format(cnt_d[False]))
+        print("The number of True HGVS IDs in the collection \
+is : {0}".format(cnt_d[True]))
+        print("The number of HGVS IDs that could not be identified by \
+parser in the collection is : {0}".format(cnt_d[None]))
+
+        # print out the HGVS IDs as user defined
+        if not print_only:
+            return out
