a0060f98565ab6cc990e4df64bc4cd6250b440ae
Chunlei Wu <anewgene@yahoo.com>
2015-04-14 18:15:44 -0700

    minor tweak in cadd mapping.

--- src/dataload/contrib/cadd/__init__.py
+++ src/dataload/contrib/cadd/__init__.py
@@ -69,7 +69,7 @@ def get_mapping():
                             "type": "string",
                             "analyzer": "string_lowercase"
                         },
-                        "gene_name": {
+                        "genename": {
                             "type": "string",
                             "analyzer": "string_lowercase"
                         }
