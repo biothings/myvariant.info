3e7ef6653fdf984edd263d5ff275b232d3ff1484
kevinxin90 <kevinxin@scripps.edu>
2015-04-15 10:45:27 -0700

    modify gwas_parser to exclude non-float value of AlleleFreq

--- src/dataload/contrib/gwassnps/gwas_parser.py
+++ src/dataload/contrib/gwassnps/gwas_parser.py
@@ -8,52 +8,110 @@ import gc
 import MySQLdb
 import requests
 
+from utils.common import loadobj, is_float
 
-def load_data(step=1000, offset=0):
-    MySQLHG19 = MySQLdb.connect('genome-mysql.cse.ucsc.edu',
-                                db='hg19', user='genomep', passwd='password')
-    Cursor = MySQLHG19.cursor()
 
-    # get the row number of gwasCatalog
-    sql = "SELECT COUNT(*) FROM gwasCatalog"
-    Cursor.execute(sql)
-    numrows = Cursor.fetchone()[0]
-    print(numrows)
+def load_data(step=1000, offset=0, gwas_data_local=None):
+    if gwas_data_local:
+        gwas_data = loadobj('gwasdata.pyobj')
+        for item in gwas_data:
+            snp = item
+            chrom = snp[1]
+            chrom = chrom[3:]
+            chromStart = int(snp[2]) + 1
+            chromEnd = int(snp[3]) + 1
+            rsid = snp[4]
+            pubMedID = snp[5]
+            title = snp[9]
+            trait = snp[10]
+            region = snp[13]
+            gene_name = snp[14]
+            riskAllele = snp[15]
+            riskAlleleFreq = snp[16]
+            if not is_float(riskAlleleFreq):
+                riskAlleleFreq = None
+            pValue = snp[17]
+            pValue_desc = snp[18]
+            # parse from myvariant.info to get hgvs_id, 
+            #ref, alt information based on rsid
+            url = 'http://localhost:8000/v1/query?q=dbsnp.rsid:'\
+                   + rsid + '&fields=_id,dbsnp.ref,dbsnp.alt,dbsnp.chrom,dbsnp.hg19'
+            r = requests.get(url)
+            for hits in r.json()['hits']:
+                HGVS = hits['_id']
 
-    sql = "SELECT * FROM gwasCatalog"
-    Cursor.execute(sql)
+                one_snp_json = {
+                    "_id": HGVS,
+                    "gwassnp":
+                        {
+                            "rsid": rsid,
+                            "pubmed": pubMedID,
+                            "title": title,
+                            "trait": trait,
+                            "region": region,
+                            "genename": gene_name,
+                            "risk_allele": riskAllele,
+                            "risk_allele_freq": riskAlleleFreq,
+                            "pvalue": pValue,
+                            "pvalue_desc": pValue_desc
+                        }
+                }
+                yield one_snp_json
+    else:
+        MySQLHG19 = MySQLdb.connect('genome-mysql.cse.ucsc.edu',
+                                    db='hg19', user='genomep', passwd='password')
+        Cursor = MySQLHG19.cursor()
 
-    for i in range(numrows):
-        snp = Cursor.fetchone()
-        if i and i % step == 0:
-            print(i)
+        # get the row number of gwasCatalog
+        sql = "SELECT COUNT(*) FROM gwasCatalog"
+        Cursor.execute(sql)
+        numrows = Cursor.fetchone()[0]
+        print(numrows)
 
-        chrom = snp[1]
-        chrom = chrom[3:]
-        chromStart = int(snp[2]) + 1
-        chromEnd = int(snp[3]) + 1
-        rsid = snp[4]
-        pubMedID = snp[5]
-        trait = snp[9]
-        riskAllele = snp[14]
-        pValue = snp[16]
-        # parse from myvariant.info to get hgvs_id, ref, alt information based on rsid
-        url = 'http://localhost:8000/v1/query?q=dbsnp.rsid:'\
-               + rsid + '&fields=_id,dbsnp.ref,dbsnp.alt,dbsnp.chrom,dbsnp.hg19'
-        r = requests.get(url)
-        for hits in r.json()['hits']:
-            HGVS = hits['_id']
-            
-            one_snp_json = {
-            # plus 'gwas'
-                "_id": HGVS,
-                "gwassnp":
-                    {
-                        "rsid": rsid,
-                        "pubmed": pubMedID,
-                        "trait": trait,
-                        "risk_allele": riskAllele,
-                        "pvalue": pValue,
-                    }
-            }
-            yield one_snp_json
+        sql = "SELECT * FROM gwasCatalog"
+        Cursor.execute(sql)
+
+        for i in range(numrows):
+            snp = Cursor.fetchone()
+            if i and i % step == 0:
+                print(i)
+
+            chrom = snp[1]
+            chrom = chrom[3:]
+            chromStart = int(snp[2]) + 1
+            chromEnd = int(snp[3]) + 1
+            rsid = snp[4]
+            pubMedID = snp[5]
+            title = snp[9]
+            trait = snp[10]
+            region = snp[13]
+            gene_name = snp[14]
+            riskAllele = snp[15]
+            riskAlleleFreq = snp[16]
+            if not is_float(riskAlleleFreq):
+                riskAlleleFreq = None
+            pValue = snp[17]
+            pValue_desc = snp[18]
+            # parse from myvariant.info to get hgvs_id, ref, alt information based on rsid
+            url = 'http://localhost:8000/v1/query?q=dbsnp.rsid:'\
+                   + rsid + '&fields=_id,dbsnp.ref,dbsnp.alt,dbsnp.chrom,dbsnp.hg19'
+            r = requests.get(url)
+            for hits in r.json()['hits']:
+                HGVS = hits['_id']
+                one_snp_json = {
+                    "_id": HGVS,
+                    "gwassnp":
+                        {
+                            "rsid": rsid,
+                            "pubmed": pubMedID,
+                            "title": title,
+                            "trait": trait,
+                            "region": region,
+                            "genename": gene_name,
+                            "risk_allele": riskAllele,
+                            "risk_allele_freq": riskAlleleFreq,
+                            "pvalue": pValue,
+                            "pvalue_desc": pValue_desc
+                        }
+                }
+                yield one_snp_json
