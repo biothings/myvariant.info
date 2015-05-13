c0058d39acd949e1711e61b987decab06530838b
kevinxin90 <kevinxin@scripps.edu>
2015-05-13 13:56:07 -0700

    add variant type function in hgvs

--- src/utils/hgvs.py
+++ src/utils/hgvs.py
@@ -48,34 +48,43 @@ def reverse_complement_hgvs(hgvs_id):
         raise ValueError("Not a Valid HGVS ID")
 
 
-def get_hgvs_from_vcf(chr, pos, ref, alt):
+def get_hgvs_from_vcf(chr, pos, ref, alt, mutant_type = None):
     '''get a valid hgvs name from VCF-style "chr, pos, ref, alt" data.'''
     if len(ref) == len(alt) == 1:
         # this is a SNP
         hgvs = 'chr{0}:g.{1}{2}>{3}'.format(chr, pos, ref, alt)
+        var_type = 'snp'
     elif len(ref) > 1 and len(alt) == 1:
         # this is a deletion:
         if ref[0] == alt:
             start = int(pos) + 1
             end = int(pos) + len(ref) - 1
             hgvs = 'chr{0}:g.{1}_{2}del'.format(chr, start, end)
+            var_type = 'del'
 	else:
 	    end = int(pos) + len(ref) - 1
             hgvs = 'chr{0}:g.{1}_{2}delins{3}'.format(chr, pos, end, alt)
+            var_type = 'delins'
     elif len(ref) == 1 and len(alt) > 1:
         # this is a insertion
         if alt[0] == ref:
             hgvs = 'chr{0}:g.{1}_{2}ins'.format(chr, pos, int(pos) + 1)
             ins_seq = alt[1:]
             hgvs += ins_seq
+            var_type = 'ins'
 	else:
 	    hgvs = 'chr{0}:g.{1}delins{2}'.format(chr, pos, alt)
+        var_type = 'delins'
     elif len(ref) > 1 and len(alt) > 1:
         end = int(pos) + len(alt) - 1
         hgvs = 'chr{0}:g.{1}_{2}delins{3}'.format(chr, pos, end, alt)
+        var_type = 'delins'
     else:
         raise ValueError("Cannot convert {} into HGVS id.".format((chr, pos, ref, alt)))
-    return hgvs
+    if mutant_type:
+        return hgvs, var_type
+    else:
+        return hgvs
 
 
 def get_pos_start_end(chr, pos, ref, alt):
