dafed67e45344410fa0a439a3262960991a934f7
Kevin Xin <kevinxin@scripps.edu>
2015-06-13 17:57:47 -0700

    Update mapping.py

--- src/dataindex/mapping.py
+++ src/dataindex/mapping.py
@@ -7,7 +7,7 @@ def get_mapping(sources=None):
         #sources = ['cadd', 'clinvar', 'cosmic2', 'dbnsfp', 'dbsnp', 'drugbank', 'emv', 'evs', 'grasp']
         sources = sources or ['dbsnp', "cadd", "evs", "snpedia", "wellderly",
                               'dbnsfp', 'emv', 'mutdb', 'docm', 'cosmic',
-                              'clinvar', 'gwassnps']
+                              'clinvar', 'gwassnps','exac','grasp']
         # extra_mapping_li = [mapping_snpedia, mapping_wellderly]
         extra_mapping_li = []
     else:
