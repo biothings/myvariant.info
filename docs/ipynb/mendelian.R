4d747f506aa318401e32d4fb72942835e5525560
Adam Mark <Adam@Adams-MacBook-Pro-2.local>
2015-08-10 12:47:41 -0700

    added jupyter R ipynb

---
+++ docs/ipynb/mendelian.R
@@ -0,0 +1,54 @@
+#library(myvariant)
+#library(mygene)
+library(magrittr)
+library(S4Vectors)
+library(VariantAnnotation)
+library(plyr)
+
+replaceWith0 <- function(df){
+  d <- data.frame(df)
+  d[is.na(d)] <- 0
+  DataFrame(d)
+}
+
+## apply filters to dataframe
+filterDf <- function(df){
+  df <- subset(df, DP > 15 & FS < 30 & QD > 2)
+  df <- subset(df, cadd.consequence %in% c("NON_SYNONYMOUS", "STOP_GAINED", "STOP_LOST", "CANONICAL_SPLICE", "SPLICE_SITE"))
+  df <- subset(df, exac.af < 0.01)
+  #df <- subset(df, dbsnp.dbsnp_build > 128 )
+  df <- subset(df, sapply(dbnsfp.1000gp1.af, function(i) i < 0.01 ))
+  df
+}
+
+## get specific gene's rows of data.frame
+geneInDf <- function(df, gene){
+  gene.df <- subset(df, sapply(df$dbnsfp.genename, function(i) gene %in% i))
+  gene.df
+}
+
+max.of.df <- function(df){
+    #return(data.frame(subset(df, cadd.phred == max(df$cadd.phred))))
+    return(data.frame(subset(df, cadd.phred == median(df$cadd.phred))))
+}
+
+## create unique dataframes by gene, check pathogenicity
+geneDf <- function(vars.list, gene){
+  patho <- lapply(vars.list, function(i) geneInDf(i, gene))
+  patho <- patho[sapply(patho, function(i) nrow(i) > 0)]
+  common <- do.call(rbind.fill, lapply(patho, max.of.df))
+  df <- list("gene"=as.character(gene),
+             "cadd.phred"=mean(common$cadd.phred))
+  df
+}
+
+rankByCaddScore <- function(gene.list, df.list){
+  y <- do.call(rbind, lapply(gene.list, function(i) geneDf(df.list, i)))
+  df <- data.frame(gene=unlist(y[,1]), cadd.phred=unlist(y[,2]))
+  ranked <- arrange(df, -cadd.phred)
+  data.frame(subset(ranked, gene != c("NULL", 0)), row.names=NULL)
+}
+
+.collapse <- function(...) {
+  paste(unlist(list(...)), sep=",", collapse=",")
+}
---
+++ docs/ipynb/miller.R
@@ -0,0 +1,29 @@
+library(myvariant)
+library(mygene)
+library(VariantAnnotation)
+
+setwd("~/sulab/myvariant/vcf/")
+vcf.files <- paste(getwd(), list.files(getwd()), sep="/")
+
+getVars <- function(vcf.file){
+  vcf <- readVcf(vcf.file, genome="hg19")
+  vcf <- vcf[isSNV(vcf)]
+  vars <- rowRanges(vcf)
+  vars <- as(vars, "DataFrame")
+  vars$query <- formatHgvs(vcf, "snp")
+  annotations <- getVariants(vars$query)
+  annotations[c('DP', 'FS', 'QD')] <- info(vcf)[c('DP', 'FS', 'QD')]
+  annotations <- replaceWith0(annotations)
+  annotations
+}
+
+vars <- lapply(vcf.files, getVars)
+snps <- lapply(vars, function(i) subset(i, DP > 15 & FS < 30 & QD > 2))
+filtered.annotations <- lapply(vars.li, filterDf)
+
+gene.counts <- data.frame(table(unlist(lapply(filtered.annotations, function(i) unique(i$dbnsfp.genename)))))
+top.genes <- subset(gene.counts, Freq == 4)
+top.genes
+
+ranked <- rankByCaddScore(top.genes$Var1, filtered.annotations)
+ranked
