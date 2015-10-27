a29c70067bf1fe9941e6af0e4dc8e88ac35b886c
Kevin Xin <kevinxin@scripps.edu>
2015-10-27 14:28:28 -0700

    Update __init__.py

--- src/dataload/contrib/docm/__init__.py
+++ src/dataload/contrib/docm/__init__.py
@@ -129,7 +129,7 @@ def get_mapping():
                 },
                 "genename": {
                     "type": "string",
-                    "analyzer": "string_lowercase"
+                    "analyzer": "string_lowercase",
                     "include_in_all": True
                 },
                 "ucsc_cons": {
