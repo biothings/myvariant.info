4e64eb54039fc010f11be087f08f4a760f89d827
Kevin Xin <kevinxin@scripps.edu>
2016-01-15 11:22:53 -0800

    add main method for mongo sync

--- src/dataload/sync.py
+++ src/dataload/sync.py
@@ -1,4 +1,5 @@
 from utils.mongo import get_src_db
+from utils.common import loadobj
 
 
 class MongoSync():
@@ -17,3 +18,13 @@ class MongoSync():
                 self._src[merge_collection].remove(_id)
             else:
                 self._src[merge_collection].update({'_id': _id}, {'$unset': {field: 1}})
+
+    def main(self, diff_filepath, merge_collection, field):
+        diff = loadobj(diff_filepath)
+        source_collection = diff['source']
+        add_ids = diff['add']
+        delete_ids = diff['delete']
+        update_ids = [_doc['_id'] for _doc in diff['update']]
+        self.add_update(source_collection, merge_collection, add_ids)
+        self.add_update(source_collection, merge_collection, update_ids)
+        self.delete(merge_collection, field, delete_ids)
