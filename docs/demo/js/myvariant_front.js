86e31e71750adc78a5b9e21777d9152165f8b5fa
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2016-02-03 21:25:24 -0800

    Changed expand/collapse buttons a little.

--- docs/demo/js/myvariant_front.js
+++ docs/demo/js/myvariant_front.js
@@ -19,6 +19,7 @@ function successHandler(data, textStatus, jqXHR) {
     jQuery('.json-panel').remove();
     jQuery('.json-view').remove();
     jQuery('.results').html("<div class='json-panel'><button id='expand-json'>Expand</button><button id='collapse-json'>Collapse</button></div><div class='json-view'></div>").show();
+    jQuery('.json-panel button').button();
     jQuery('.json-view').JSONView(data); //, {collapsed: true});
     jQuery('.json-view').JSONView('expand');
     jQuery('#expand-json').click(function() {jQuery('.json-view').JSONView('expand');});
