7a31fd8a8ba784ee12fa9cb423c47adc9b4f27a3
Cyrus Afrasiabi <cyrusafrasiabi@wireless-121-206.scripps.edu>
2016-04-06 11:53:34 -0700

    Base biothings.variant project.

--- src/www/api/handlers.py
+++ src/www/api/handlers.py
@@ -1,224 +1,50 @@
-import re
-import json
-
-from tornado.web import HTTPError
-from www.helper import BaseHandler
-from .es import ESQuery
-from utils.common import split_ids
+# -*- coding: utf-8 -*-
+from biothings.www.api.handlers import MetaDataHandler, BiothingHandler, QueryHandler, StatusHandler, FieldsHandler
+from biothings.settings import BiothingSettings
+from www.api.es import ESQuery
 import config
 
+biothing_settings = BiothingSettings()
 
-class VariantHandler(BaseHandler):
+class VariantHandler(BiothingHandler):
+    ''' This class is for the /variant endpoint. '''
     esq = ESQuery()
 
-    def get(self, vid=None):
-        '''
-        /variant/<variantid>
-            varintid can be HGVS name.
-        /variant/chr1:g.160145907G>T
-        /variant/chr1:g.160145907G>T?fields=dbsnp
-        /variant/chr1:g.160145907G>T?fields=dbnsfp.genename,dbnsfp.cadd
-
-        parameters:
-            fields
-            callback
-            email
-        '''
-        if vid:
-            # Get rid of HGVS ID errors
-            m = re.search('chr.{1,2}(?P<delim>:[g\.]{0,2})\d+', self.request.uri)
-            if m:
-                de = m.group('delim')
-                if de and de != ':g.':
-                    self.redirect(':g.'.join(self.request.uri.split(de)), permanent=True)
-            # Test for HGVS formatting errors
-            kwargs = self.get_query_params()
-            self.esq._use_hg19()
-            if kwargs.pop('assembly', 'hg19').lower() == 'hg38':
-                self.esq._use_hg38()
-            variant = self.esq.get_variant(vid, **kwargs)
-            if variant:
-                self.return_json(variant)
-                self.ga_track(event={'category': 'v1_api',
-                                     'action': 'variant_get'})
-            else:
-                raise HTTPError(404)
-        else:
-            raise HTTPError(404)
-
-    def post(self, ids=None):
-        '''
-           This is essentially the same as post request in QueryHandler, with different defaults.
-
-           parameters:
-            ids
-            fields
-            email
-        '''
-        kwargs = self.get_query_params()
-        self.esq._use_hg19()
-        if kwargs.pop('assembly', 'hg19').lower() == 'hg38':
-            self.esq._use_hg38()
-        ids = kwargs.pop('ids', None)
-        if ids:
-            ids = re.split('[\s\r\n+|,]+', ids)
-            res = self.esq.mget_variants2(ids, **kwargs)
-        else:
-            res = {'success': False, 'error': "Missing required parameters."}
-        encode = not isinstance(res, str)    # when res is a string, e.g. when rawquery is true, do not encode it as json
-        self.return_json(res, encode=encode)
-        self.ga_track(event={'category': 'v1_api',
-                             'action': 'variant_post',
-                             'label': 'qsize',
-                             'value': len(ids) if ids else 0})
-
-
-class QueryHandler(BaseHandler):
+class QueryHandler(QueryHandler):
+    ''' This class is for the /query endpoint. '''
     esq = ESQuery()
 
-    def get(self):
-        '''
-        parameters:
-            q
-            fields
-            from
-            size
-            sort
-            facets
-            callback
-            email
-            fetch_all
-
-            explain
-            raw
-        '''
-        kwargs = self.get_query_params()
-        self.esq._use_hg19()
-        if kwargs.pop('assembly', 'hg19').lower() == 'hg38':
-            self.esq._use_hg38()
-        q = kwargs.pop('q', None)
-        scroll_id = kwargs.pop('scroll_id', None)
-        _has_error = False
-        if scroll_id:
-            res = self.esq.scroll(scroll_id, **kwargs)
-        elif q:
-            for arg in ['from', 'size']:
-                value = kwargs.get(arg, None)
-                if value:
-                    try:
-                        kwargs[arg] = int(value)
-                    except ValueError:
-                        res = {'success': False, 'error': 'Parameter "{}" must be an integer.'.format(arg)}
-                        _has_error = True
-            if not _has_error:
-                res = self.esq.query(q, **kwargs)
-                if kwargs.get('fetch_all', False):
-                    self.ga_track(event={'category': 'v1_api',
-                                         'action': 'fetch_all',
-                                         'label': 'total',
-                                         'value': res.get('total', None)})
-
-        else:
-            res = {'success': False, 'error': "Missing required parameters."}
-
-        self.return_json(res)
-        self.ga_track(event={'category': 'v1_api',
-                             'action': 'query_get',
-                             'label': 'qsize',
-                             'value': len(q) if q else 0})
-
-    def post(self):
-        '''
-        parameters:
-            q
-            scopes
-            fields
-            email
-
-            jsoninput   if true, input "q" is a json string, must be decoded as a list.
-        '''
-        kwargs = self.get_query_params()
-        self.esq._use_hg19()
-        if kwargs.pop('assembly', 'hg19').lower() == 'hg38':
-            self.esq._use_hg38()
-        q = kwargs.pop('q', None)
-        jsoninput = kwargs.pop('jsoninput', None) in ('1', 'true')
-        if q:
-            # ids = re.split('[\s\r\n+|,]+', q)
-            try:
-                ids = json.loads(q) if jsoninput else split_ids(q)
-                if not isinstance(ids, list):
-                    raise ValueError
-            except ValueError:
-                ids = None
-                res = {'success': False, 'error': 'Invalid input for "q" parameter.'}
-            if ids:
-                scopes = kwargs.pop('scopes', None)
-                fields = kwargs.pop('fields', None)
-                res = self.esq.mget_variants2(ids, fields=fields, scopes=scopes, **kwargs)
-        else:
-            res = {'success': False, 'error': "Missing required parameters."}
-
-        encode = not isinstance(res, str)    # when res is a string, e.g. when rawquery is true, do not encode it as json
-        self.return_json(res, encode=encode)
-        self.ga_track(event={'category': 'v1_api',
-                             'action': 'query_post',
-                             'label': 'qsize',
-                             'value': len(q) if q else 0})
-
-
-class MetaDataHandler(BaseHandler):
-    esq = ESQuery()
-    disable_caching = True
-
-    def get(self):
-        _meta = self.esq.get_mapping_meta()
-        self.return_json(_meta)
-
-
-class FieldsHandler(BaseHandler):
+class StatusHandler(StatusHandler):
+    ''' This class is for the /status endpoint. '''
     esq = ESQuery()
 
-    def get(self):
-        notes = json.load(open(config.FIELD_NOTES_PATH, 'r'))
-        es_mapping = self.esq.query_fields()
-        kwargs = self.get_query_params()
+class FieldsHandler(FieldsHandler):
+    ''' This class is for the /metadata/fields endpoint. '''
+    esq = ESQuery()
 
-        def get_indexed_properties_in_dict(d, prefix):
-            r = {}
-            for (k, v) in d.items():
-                r[prefix + '.' + k] = {}
-                r[prefix + '.' + k]['indexed'] = False
-                if 'properties' not in v:
-                    r[prefix + '.' + k]['type'] = v['type']
-                    if ('index' not in v) or ('index' in v and v['index'] != 'no'):
-                        # indexed field
-                        r[prefix + '.' + k]['indexed'] = True
-                else:
-                    r[prefix + '.' + k]['type'] = 'object'
-                    r.update(get_indexed_properties_in_dict(v['properties'], prefix + '.' + k))
-                if ('include_in_all' in v) and v['include_in_all']:
-                    r[prefix + '.' + k]['include_in_all'] = True
-                else:
-                    r[prefix + '.' + k]['include_in_all'] = False
-            return r
-
-        r = {}
-        search = kwargs.pop('search', None)
-        prefix = kwargs.pop('prefix', None)
-        for (k, v) in get_indexed_properties_in_dict(es_mapping, '').items():
-            k1 = k.lstrip('.')
-            if (search and search in k1) or (prefix and k1.startswith(prefix)) or (not search and not prefix):
-                r[k1] = v
-                if k1 in notes:
-                    r[k1]['notes'] = notes[k1]
-        self.return_json(r)
+class MetaDataHandler(MetaDataHandler):
+    ''' This class is for the /metadata endpoint. '''
+    esq = ESQuery()
 
 
-APP_LIST = [
-    (r"/variant/(.+)/?", VariantHandler),   # for variant get request
-    (r"/variant/?$", VariantHandler),       # for variant post request
-    (r"/query/?", QueryHandler),            # for query get/post request
-    (r"/metadata", MetaDataHandler),        # for metadata requests
-    (r"/metadata/fields", FieldsHandler),   # for available field information
-]
+def return_applist():
+    ret = [
+        (r"/status", StatusHandler),
+        (r"/metadata", MetaDataHandler),
+        (r"/metadata/fields", FieldsHandler),
+    ]
+    if biothing_settings._api_version:
+        ret += [
+            (r"/" + biothing_settings._api_version + "/metadata", MetaDataHandler),
+            (r"/" + biothing_settings._api_version + "/metadata/fields", FieldsHandler),
+            (r"/" + biothing_settings._api_version + "/variant/(.+)/?", VariantHandler),
+            (r"/" + biothing_settings._api_version + "/variant/?$", VariantHandler),
+            (r"/" + biothing_settings._api_version + "/query/?", QueryHandler),
+        ]
+    else:
+        ret += [
+            (r"/variant/(.+)/?", VariantHandler),
+            (r"/variant/?$", VariantHandler),
+            (r"/query/?", QueryHandler),
+        ]
+    return ret
--- src/www/index.py
+++ src/www/index.py
@@ -1,111 +1,14 @@
-'''
-A thin python layer for accessing MyVariant ElasticSearch host.
-
-Currently available URLs:
-
-    /v1/query?q=rs58991260            variant query service
-    /v1/variant/<variant_id>    variant annotation service
-
-'''
+# -*- coding: utf-8 -*-
+# Simple template example used to instantiate a new biothing API
 import sys
 import os.path
-#import subprocess
-#import json
-
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-import tornado.escape
-from tornado.options import define, options
 
 src_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
 if src_path not in sys.path:
     sys.path.append(src_path)
-#from config import INCLUDE_DOCS
 
-from www.api.es import ESQuery
-from www.helper import add_apps
-from www.api.handlers import APP_LIST as api_app_list
-from www.api.handlers import MetaDataHandler
-from www.api.handlers import FieldsHandler
-from www.beacon.handlers import APP_LIST as beacon_app_list
+from biothings.www.index_base import main
+from www.api.handlers import return_applist
 
-__USE_WSGI__ = False
-#DOCS_STATIC_PATH = os.path.join(src_path, 'docs/_build/html')
-#if INCLUDE_DOCS and not os.path.exists(DOCS_STATIC_PATH):
-#    raise IOError('Run "make html" to generate sphinx docs first.')
-STATIC_PATH = os.path.join(src_path, 'www/static')
-
-define("port", default=8000, help="run on the given port", type=int)
-define("address", default="127.0.0.1", help="run on localhost")
-define("debug", default=False, type=bool, help="run in debug mode")
-tornado.options.parse_command_line()
-if options.debug:
-    import tornado.autoreload
-    import logging
-    logging.getLogger().setLevel(logging.DEBUG)
-    options.address = '0.0.0.0'
-
-
-class StatusCheckHandler(tornado.web.RequestHandler):
-    ''' Handles requests to check the status of the server. '''
-    def head(self):
-        esq = ESQuery()
-        esq = esq.get_variant('chr1:g.218631822G>A')
-
-    def get(self):
-        self.head()
-        self.write('OK')
-
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        #if INCLUDE_DOCS:
-            self.render(os.path.join(STATIC_PATH, 'index.html'))
-
-
-class DemoHandler(tornado.web.RequestHandler):
-    ''' Redirect to the rawgit address for the demo page. '''
-    def get(self):
-        with open('../../docs/demo/index.html', 'r') as demo_file:
-            self.write(demo_file.read())
-
-APP_LIST = [
-    (r"/", MainHandler),
-    (r"/status", StatusCheckHandler),
-    (r"/metadata", MetaDataHandler),
-    (r"/metadata/fields", FieldsHandler),
-    (r"/demo/?$", DemoHandler),
-]
-
-APP_LIST += add_apps('api', api_app_list)
-APP_LIST += add_apps('v1', api_app_list)
-APP_LIST += add_apps('beacon', beacon_app_list)
-
-
-settings = {}
-if options.debug:
-    # from config import STATIC_PATH
-    settings.update({
-        "static_path": STATIC_PATH,
-    })
-    # from config import auth_settings
-    # settings.update(auth_settings)
-
-
-def main():
-    application = tornado.web.Application(APP_LIST, **settings)
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port, address=options.address)
-    loop = tornado.ioloop.IOLoop.instance()
-    if options.debug:
-        tornado.autoreload.start(loop)
-        tornado.autoreload.watch(os.path.join(STATIC_PATH, 'index.html'))
-        logging.info('Server is running on "%s:%s"...' % (options.address, options.port))
-
-    loop.start()
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    main(return_applist())
