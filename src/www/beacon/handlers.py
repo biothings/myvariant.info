db44b7dd96c73c94399f37012466d8fc6d914280
Mike Mayers <mmayers@scripps.edu>
2016-04-25 18:15:59 -0700

    Fixed issue with type error for chromosome queries. Added a new Wellderly handeler for Beacon V2 specs.

--- src/www/beacon/handlers.py
+++ src/www/beacon/handlers.py
@@ -12,7 +12,7 @@ class WellderlyBeaconHandler(BaseHandler):
         ref = self.get_argument('ref', None)
 
         if chrom and pos and allele:
-            q = 'wellderly.chr:{} AND wellderly.pos:{} AND wellderly.alt:{}'
+            q = 'wellderly.chrom:"{}" AND wellderly.pos:{} AND wellderly.alt:{}'
             q = q.format(chrom, pos, allele)
             if ref:
                 q += ' AND wellderly.ref:{}'.format(ref)
@@ -30,6 +30,31 @@ class WellderlyBeaconHandler(BaseHandler):
         self.write(out)
 
 
+class WellderlyBeaconHandler_v2(BaseHandler):
+    esq = ESQuery()
+    
+    def get(self):
+        chrom = self.get_argument('chrom', None)
+        pos = self.get_argument('pos', None)
+        allele = self.get_argument('allele', None)
+        assembly = self.get_argument('assembly', default='GRCh37')
+                
+        #Initialize Output
+        out = {'exists':False, 'metadata':None}        
+
+        # check if assembly is in GRCh37 or GRCh38
+        if chrom and pos and allele and assembly in ['GRCh37', 'GRCh38']:
+            q = 'wellderly.chrom:"{}" AND wellderly.pos:{} AND wellderly.alt:{}'
+            q = q.format(chrom, pos, allele)
+            res = self.esq.query(q)
+            if res and res.get('total') > 0:
+                out['exists'] = True
+                out['metadata'] = [hit['wellderly'] for hit in res.get('hits')]
+            
+        self.return_json(out)
+                
+
 APP_LIST = [
     (r"/wellderly/?", WellderlyBeaconHandler),
+    (r"/wellderly2/?", WellderlyBeaconHandler_v2),
 ]
