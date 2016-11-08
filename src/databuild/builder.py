d9a6c6aecd051cbe9c87350d3e43a22dc97b5bf7
Sebastien Lelong <slelong@scripps.edu>
2016-11-08 11:57:10 -0800

    bin/build.py helper script + MyVariant specific builder class (post_merge validation is different)

--- src/databuild/builder.py
+++ src/databuild/builder.py
@@ -2,44 +2,16 @@
 import biothings.databuild.builder as builder
 
 class MyVariantDataBuilder(builder.DataBuilder):
-    pass
 
-
-if __name__ == '__main__':
-    import sys
-
-    import biothings, config
-    biothings.config_for_app(config)
-    from config import LOG_FOLDER
-
-    import biothings.utils.mongo as mongo
-    import biothings.databuild.backend as btbackend
-
-    build_name = "myvariant"
-    use_parallel = '-p' in sys.argv
-    sources = None  # will build all sources
-    target = None   # will generate a new collection name
-    # "target_col:src_col1,src_col2" will specifically merge src_col1
-    # and src_col2 into existing target_col (instead of merging everything)
-    if not use_parallel and len(sys.argv) > 2:
-        target,tmp = sys.argv[2].split(":")
-        sources = tmp.split(",")
-
-    # declare source backend
-    source_backend =  btbackend.SourceDocMongoBackend(
-                            build=mongo.get_src_build(),
-                            master=mongo.get_src_master(),
-                            dump=mongo.get_src_dump(),
-                            sources=mongo.get_src_db())
-    # declare target backend
-    target_backend = btbackend.TargetDocMongoBackend(target_db=mongo.get_target_db())
-    # assemble the whole
-    bdr = MyVariantDataBuilder(
-            build_name,
-            doc_root_key=None,
-            source_backend=source_backend,
-            target_backend=target_backend,
-            log_folder=config.LOG_FOLDER)
-    # and start merging process
-    bdr.merge(sources=sources,target_name=target)
+    def post_merge(self):
+        # MyVariant merging either insert or updates. So we can't just count
+        # the number of inserted/updated data from single colleciton and compare with
+        # the target count.
+        total = sum(self.stats.values())
+        self.logger.info("Validating...")
+        target_cnt = self.target_backend.count()
+        if target_cnt <= total:
+            self.logger.info("OK [total count={} <= sum(total)={}]".format(target_cnt,total))
+        else:
+            self.logger.info("Warning: total count of documents {} is greater than what was inserted/updated... {}]".format(target_cnt, total))
 
