cb96728da5a332c171c3e8c0944c0c639c57ff3f
Sebastien Lelong <slelong@scripps.edu>
2017-01-05 23:21:02 -0800

    wellderly as a single collection source (not using cg version)

--- src/dataload/sources/wellderly/wellderly_upload.py
+++ src/dataload/sources/wellderly/wellderly_upload.py
@@ -6,22 +6,21 @@ class WellderlyFactoryUploader(uploader.DummySourceUploader,SnpeffPostUpdateUplo
     """Data originally coming from: http://www.stsiweb.org/wellderly"""
 
     name = "wellderly"
-    regex_name = "wellderly_.*"    # this will be compiled as regex to match split cols
     __metadata__ = {"mapper" : 'observed',
                     "assembly" : "hg19"}
-    split_collections = ["wellderly_cg1","wellderly_cg10","wellderly_cg11",
-                         "wellderly_cg12","wellderly_cg13","wellderly_cg14",
-                         "wellderly_cg15","wellderly_cg16","wellderly_cg17",
-                         "wellderly_cg18","wellderly_cg19","wellderly_cg2",
-                         "wellderly_cg20","wellderly_cg21","wellderly_cg22",
-                         "wellderly_cg3","wellderly_cg4","wellderly_cg5",
-                         "wellderly_cg6","wellderly_cg7","wellderly_cg8",
-                         "wellderly_cg9","wellderly_cgX","wellderly_cgY",
-                         "wellderly_cgY1"]
+    #split_collections = ["wellderly_cg1","wellderly_cg10","wellderly_cg11",
+    #                     "wellderly_cg12","wellderly_cg13","wellderly_cg14",
+    #                     "wellderly_cg15","wellderly_cg16","wellderly_cg17",
+    #                     "wellderly_cg18","wellderly_cg19","wellderly_cg2",
+    #                     "wellderly_cg20","wellderly_cg21","wellderly_cg22",
+    #                     "wellderly_cg3","wellderly_cg4","wellderly_cg5",
+    #                     "wellderly_cg6","wellderly_cg7","wellderly_cg8",
+    #                     "wellderly_cg9","wellderly_cgX","wellderly_cgY",
+    #                     "wellderly_cgY1"]
 
-    @classmethod
-    def create(klass, db_conn_info, data_root, *args, **kwargs):
-        return [klass(db_conn_info, data_root, collection_name=c,*args,*kwargs) for c in klass.split_collections]
+    #@classmethod
+    #def create(klass, db_conn_info, data_root, *args, **kwargs):
+    #    return [klass(db_conn_info, data_root, collection_name=c,*args,*kwargs) for c in klass.split_collections]
 
     @classmethod
     def get_mapping(klass):
