94ca63458a57c466aa818464c7093f76b2237a3c
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-01-07 14:52:29 -0800

    Base new myvariant repo.

---
+++ src/config_common.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from biothings.www.settings.default import *
+from www.api.query_builder import ESQueryBuilder
+from www.api.query import ESQuery
+from www.api.transform import ESResultTransformer
+from www.api.handlers import VariantHandler, QueryHandler, MetadataHandler, StatusHandler
+
+# *****************************************************************************
+# Elasticsearch variables
+# *****************************************************************************
+# elasticsearch server transport url
+ES_HOST = 'localhost:9200'
+# elasticsearch index name
+ES_INDEX = 'myvariant_current'
+# elasticsearch document type
+ES_DOC_TYPE = 'variant'
+
+API_VERSION = 'v1'
+
+# *****************************************************************************
+# App URL Patterns
+# *****************************************************************************
+APP_LIST = [
+    (r"/status", StatusHandler),
+    (r"/metadata/?", MetadataHandler),
+    (r"/metadata/fields/?", MetadataHandler),
+    (r"/{}/variant/(.+)/?".format(API_VERSION), VariantHandler),
+    (r"/{}/variant/?$".format(API_VERSION), VariantHandler),
+    (r"/{}/query/?".format(API_VERSION), QueryHandler),
+    (r"/{}/metadata/?".format(API_VERSION), MetadataHandler),
+    (r"/{}/metadata/fields/?".format(API_VERSION), MetadataHandler),
+]
+
+###############################################################################
+#   app-specific query builder, query, and result transformer classes
+###############################################################################
+
+# *****************************************************************************
+# Subclass of biothings.www.api.es.query_builder.ESQueryBuilder to build
+# queries for this app
+# *****************************************************************************
+ES_QUERY_BUILDER = ESQueryBuilder
+# *****************************************************************************
+# Subclass of biothings.www.api.es.query.ESQuery to execute queries for this app
+# *****************************************************************************
+ES_QUERY = ESQuery
+# *****************************************************************************
+# Subclass of biothings.www.api.es.transform.ESResultTransformer to transform
+# ES results for this app
+# *****************************************************************************
+ES_RESULT_TRANSFORMER = ESResultTransformer
+
+GA_ACTION_QUERY_GET = 'query_get'
+GA_ACTION_QUERY_POST = 'query_post'
+GA_ACTION_ANNOTATION_GET = 'variant_get'
+GA_ACTION_ANNOTATION_POST = 'variant_post'
+GA_TRACKER_URL = 'MyVariant.info'
+
+STATUS_CHECK_ID = ''
--- src/www/beacon/handlers.py
+++ src/www/beacon/handlers.py
@@ -1,35 +1,180 @@
-from www.helper import BaseHandler
+import sys
+from biothings.www.helper import BaseHandler
 from www.api.es import ESQuery
 
 
-class WellderlyBeaconHandler(BaseHandler):
+class BeaconHandler(BaseHandler):
     esq = ESQuery()
+    # Initialize Assembly and Datasets
+    assembly_keys = {'NCBI36':'hg18', 'GRCh37':'hg19', 'GRCh38':'hg38'}
+    pos_dbs = ['exac', 'cadd'] # These are hg19 ONLY
+    assembly_dbs = ['dbnsfp','dbsnp','clinvar','evs','mutdb','cosmic','docm','wellderly']
+
+    def post(self, src=None):
+        self.recieve_data()
+
+    def get(self, src=None):
+        self.recieve_data()
+
+    def recieve_data(self):
+        chrom = self.get_argument('referenceName', None)
+        start = self.get_argument('start', None)
+        ref = self.get_argument('referenceBases', None)
+        alt = self.get_argument('alternateBases', None)
+        assembly = self.get_argument('assemblyId', None)
+        datasets = self.get_arguments('datasetIds', strip=False)
+        include_datasets = self.get_argument('includeDatasetResponses', None)
+
+        # Prep request for return
+        allele_request = {'referenceName':chrom, 'start':start, 'referenceBases':ref,
+                         'alternateBases':alt, 'assemblyId': assembly, 'datsetIds': datasets,
+                         'includeDatasetResponses': include_datasets}
+
+        if len(datasets) < 1:
+            datasets = self.pos_dbs+self.assembly_dbs
+        try:
+            dataset_responses = []
+            for dataset in datasets:
+                dataset_responses.append(self.query_dataset(chrom, start, ref, alt, assembly, dataset))
+            exists = any([response['exists'] for response in dataset_responses])
+            out = {'exists':exists, 'alleleRequest': allele_request}
+            if include_datasets:
+                out['alleleDatasetRespone'] = dataset_responses
+        except NameError as e:
+            out = {'`error`':'{}'.format(e), 'alleleRequest': allele_request}
+        except TypeError as e:
+            out = {'`error`':'{}'.format(e), 'alleleRequest': allele_request}
+        except AttributeError as e:
+            out = {'`error`':'{}'.format(e), 'alleleRequest': allele_request}
+        except:
+            e = sys.exc_info()[0]
+            out = {'`error`':'{}'.format(e), 'alleleRequest': allele_request}
+
+        #Return the JSON response
+        self.return_json(out)
+
+
+    def query_dataset(self, chrom, start, ref, alt, assembly, dataset):
+        # Initialzie output
+        out = {'datasetId': dataset, 'exists':False}
+        q_type = 'snp'
+
+        # verify information and build query string
+        if dataset in self.pos_dbs+self.assembly_dbs:
+            if chrom and start and alt and assembly in self.assembly_keys:
+                assembly = self.assembly_keys[assembly]  #get hg assembly notation
+                if alt[:3] == 'DEL': # syntax: "alternateBases": "DEL85689"
+                    q_type = 'del'
+                    ref = ''
+                elif alt[:3] == 'DUP': # "alternateBases": "DUP85689"
+                    q_type = 'dup'
+                    ref = ''
+                elif not ref:
+                    q_type = 'ins'
+                    ref = ''
+
+                q = self.format_query_string(q_type, chrom, start, ref, alt, assembly, dataset)
+                # perform query and format result
+                res = self.esq.query(q, fields=dataset, dotfield=1)
+                if res and res.get('total') > 0:
+                    out = self.format_output(res, out, q_type)
+        return out
+
+
+    def format_query_string(self, q_type, chrom, start, ref, alt, assembly, dataset):
+        # Initialize some variables specific to Deletions and Duplicatoins
+        # Querying for deletions and duplcations can be done with same syntax
+        # issue then comes in resolving query result deletion or a duplication
+        # will perform query using same logic, and verify afterward
+        del_dup = ['del', 'dup']
+        if q_type in del_dup:
+            length = alt[3:]
+            end = int(start)+int(length)-1
+
+        q = dataset + '.chrom:"{}" AND '.format(chrom)
+        if dataset in self.pos_dbs and assembly == 'hg19':
+            q += dataset + '.pos:{} AND '.format(start)
+            if q_type in del_dup:
+                q += 'hg19.end:{} AND '.format(end)
+        elif dataset in self.assembly_dbs:
+            q += dataset + '.{}.start:{} AND '.format(assembly, start)
+            if q_type in del_dup:
+                q += dataset + '.{}.end:{} AND '.format(assembly, end)
+        else:
+            return ''
+        if not q_type in del_dup:
+            q += dataset + '.ref:{} AND '.format(ref)
+            q += dataset + '.alt:{} AND '.format(alt)
+        q += '_exists_:{}'.format(dataset)
+
+        return q
+
+
+    def format_output(self, res, out, q_type):
+        if q_type in ['dup', 'del']:
+            hits = [hit for hit in res.get('hits') if q_type in hit['_id']]
+        else:
+            hits = res.get('hits')
+        if len(hits) == 1:
+            out['exists'] = True
+            out['info'] = {k:v for k,v in hits[0].items() if not k.startswith('_')}
+        else:
+            out['info'] = [hit for hit in hits]
+        return out
+
+
+class BeaconInfoHandler(BaseHandler):
+    # Use esq to grab metadata on myvariant.info
+    esq = ESQuery()
+    meta = esq.get_mapping_meta()
+
+    # Access Mapping Data for later use to determine assemblyID
+    m = esq._get_mapping(index = esq._index, doc_type=esq._doc_type, options=esq._get_cleaned_metadata_options({}))
+    m = m[list(m.keys())[0]]['mappings'][esq._doc_type]['properties']
+
+    # Current list of datasets in myvariant.info
+    dataset_names = ['dbnsfp', 'dbsnp', 'clinvar', 'evs', 'cadd', 'mutdb', 'cosmic', 'docm', 'wellderly', 'exac']
+
 
     def get(self):
-        chrom = self.get_argument('chrom', None)
-        pos = self.get_argument('pos', None)
-        allele = self.get_argument('allele', None)
-        ref = self.get_argument('ref', None)
+        self.get_beacon_info()
 
-        if chrom and pos and allele:
-            q = 'wellderly.chr:{} AND wellderly.pos:{} AND wellderly.alt:{}'
-            q = q.format(chrom, pos, allele)
-            if ref:
-                q += ' AND wellderly.ref:{}'.format(ref)
-            res = self.esq.query(q, fields=None)
-            if res and res.get('total') >= 1:
-                # out = {'exist': True}
-                out = 'YES'
-            else:
-                # out = {'exist': False}
-                out = 'NO'
-        else:
-            # out = {'success': False, 'error': "Missing required parameters."}
-            out = 'NULL'
-        #self.return_json(out)
-        self.write(out)
+    def post(self):
+        self.get_beacon_info()
+
+    def get_beacon_info(self):
+        # Boilerplate Beacon Info
+        out = {'id': 'myvariant.info', 'apiVersion': 'v1', 'BeaconOrganization': 'TSRI',
+               'welcomeUrl': 'http://www.myvariant.info'}
+
+        # Loop through datasets to generate info
+        datasets = []
+        for dataset in self.dataset_names:
+            datasets.append(self.get_dataset_info(dataset))
+        out['datasets'] = datasets
+
+        # Return info
+        self.return_json(out)
 
 
-APP_LIST = [
-    (r"/wellderly/?", WellderlyBeaconHandler),
-]
+    def get_dataset_info(self, dataset):
+        #Get Basic Dataset info
+        out = {'id': dataset}
+        out['version'] = self.meta['src_version'].get(dataset, None)
+        out['variantCount'] = self.meta['stats'].get(dataset, None)
+
+        # Determine assemblies supported by dataset
+        assemblies = []
+        dataset_keys = self.m[dataset]['properties'].keys()
+
+        if 'hg18' in dataset_keys:
+            assemblies.append('NCBI36')
+        if 'hg19' in dataset_keys or dataset in ['exac', 'cadd']:
+            assemblies.append('GRCh37')
+        if 'hg38' in dataset_keys:
+            assemblies.append('GRCh38')
+
+        out['assemblyId'] = assemblies
+
+        return out
+
