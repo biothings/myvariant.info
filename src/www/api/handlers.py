e16b00763d6bf2ba5be82f040be71ac54ad18766
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2017-01-07 20:50:31 -0800

    Adding redirection of malformed hgvs ids, sanitizing assembly parameter

--- src/www/api/handlers.py
+++ src/www/api/handlers.py
@@ -3,19 +3,59 @@ from biothings.www.api.es.handlers import BiothingHandler
 from biothings.www.api.es.handlers import MetadataHandler
 from biothings.www.api.es.handlers import QueryHandler
 from biothings.www.api.es.handlers import StatusHandler
+from tornado.web import RequestHandler
+from re import search
 
-class VariantHandler(BiothingHandler):
+class CommonHandlerMixin(object):
+    def _sanitize_assembly(self, kwargs):
+        if self._should_sanitize('assembly', kwargs):
+            if kwargs['assembly'].lower() not in self.web_settings.SUPPORTED_ASSEMBLIES:
+                kwargs['assembly'] = self.esqb_kwargs['assembly']['default']
+            else:
+                kwargs['assembly'] = kwargs['assembly'].lower()
+        return kwargs
+    
+    def _get_es_index(self, options):
+        return '_'.join([self.web_settings.ES_INDEX_BASE, options.esqb_kwargs.assembly])
+
+class VariantHandler(CommonHandlerMixin, BiothingHandler):
     ''' This class is for the /variant endpoint. '''
-    pass
+    # overridden to sanitize assembly param
+    def _sanitize_params(self, kwargs):
+        kwargs = super(VariantHandler, self)._sanitize_params(kwargs)
+        kwargs = self._sanitize_assembly(kwargs)
+        return kwargs
 
-class QueryHandler(QueryHandler):
+    # redirect improperly formatted hgvs ids
+    def _regex_redirect(self, bid):
+        m = search('chr.{1,2}(?P<delim>:[g\.]{0,2})\d+', self.request.uri)
+        if m:
+            de = m.group('delim')
+            if de and de != ':g.':
+                self.redirect(':g.'.join(self.request.uri.split(de)), permanent=True)
+
+class QueryHandler(CommonHandlerMixin, QueryHandler):
     ''' This class is for the /query endpoint. '''
-    pass
+    # overridden to sanitize assembly param
+    def _sanitize_params(self, kwargs):
+        kwargs = super(QueryHandler, self)._sanitize_params(kwargs)
+        kwargs = self._sanitize_assembly(kwargs)
+        return kwargs
 
 class StatusHandler(StatusHandler):
     ''' This class is for the /status endpoint. '''
     pass
 
-class MetadataHandler(MetadataHandler):
+class MetadataHandler(CommonHandlerMixin, MetadataHandler):
     ''' This class is for the /metadata endpoint. '''
-    pass
+    # overridden to sanitize assembly param
+    def _sanitize_params(self, kwargs):
+        kwargs = super(MetadataHandler, self)._sanitize_params(kwargs)
+        kwargs = self._sanitize_assembly(kwargs)
+        return kwargs
+
+class DemoHandler(RequestHandler):
+    ''' For the /demo page. '''
+    def get(self):
+        with open('../../docs/demo/index.html', 'r') as demo_file:
+            self.write(demo_file.read())
