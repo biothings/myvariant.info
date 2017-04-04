699e51b4037e9d6c5de43dca0ff954b927c60591
Sebastien Lelong <slelong@scripps.edu>
2017-04-04 10:29:23 -0700

    moved to base class

--- src/dataindex/indexer.py
+++ src/dataindex/indexer.py
@@ -6,8 +6,6 @@ class VariantIndexer(indexer.Indexer):
     def get_mapping(self, enable_timestamp=True):
         mapping = super(VariantIndexer,self).get_mapping(enable_timestamp=enable_timestamp)
         # enrich with myvariant specific stuff
-        mapping["dynamic"] = False
-        mapping["include_in_all"] = False
         mapping["properties"]["chrom"] = {
             'analyzer': 'string_lowercase',
             'include_in_all': False,
