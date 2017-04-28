b7018fe85e19803c5c2703797b57a61b34a171ef
Sebastien Lelong <sebastien.lelong@gmail.com>
2017-04-27 18:49:12 -0700

    update metadata of "symlink" id cache file + fix permissions

--- src/dataindex/indexer.py
+++ src/dataindex/indexer.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+from boto import connect_s3
 
 import config
 import biothings.dataindex.indexer as indexer
@@ -34,7 +35,20 @@ class VariantIndexer(indexer.Indexer):
             raise FileNotFoundError("Can't find cache file '%s'" % cache_file)
         self.logger.info("Upload _id cache file '%s' to s3" % cache_file)
         try:
-            send_s3_file(cache_file,os.path.join(config.S3_FOLDER,os.path.basename(cache_file)),overwrite=True)
+            s3path = os.path.basename(cache_file)
+            send_s3_file(cache_file, s3path, overwrite=True)
+            # make the file public
+            s3 = connect_s3(config.AWS_KEY, config.AWS_SECRET) 
+            bucket = s3.get_bucket(config.S3_BUCKET)
+            s3key = bucket.get_key(s3path)
+            s3key.set_acl("public-read")
+            # update permissions and redirect metadata
+            if "hg19" in s3path:
+                k = bucket.get_key("myvariant_hg19_ids.xz")
+            else:
+                k = bucket.get_key("myvariant_hg38_ids.xz")
+            k.set_redirect("/%s" % s3path)
+            k.set_acl("public-read")
             self.logger.info("Cache file '%s' uploaded to s3" % cache_file, extra={"notify":True})
         except Exception as e:
             self.logger.error("Failed to upload cache file '%s' to s3: %s" % (cache_file,e), extra={"notify":True})
