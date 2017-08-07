9881e4a5efca2f91266ec9171410733ed16a00b6
Byung Ryul Jeon <labmed@gmail.com>
2017-08-07 15:46:39 -0700

    remove chrom, hg19, chr convert rsid to "rs"+number, remove rsid if "0" transcript, remove it if "none" codingdnasize, remove it if "-1" functionalclass remove none and blank clean up all annotation columns rewrite for loop into a generator.

--- src/dataload/sources/denovodb/denovodb_parser.py
+++ src/dataload/sources/denovodb/denovodb_parser.py
@@ -11,35 +11,17 @@ VALID_COLUMN_NO = 31
 http://denovo-db.gs.washington.edu/denovo-db/Download.jsp'''
 
 
-def get_pos_start_end_from_hgvs(hgvs, var_type, position):
-    '''get start,end tuple from hgvs name'''
-    if var_type == "snp":
-        start = position
-        end = position
-    else:
-        start_end = hgvs.split("del")[0].split("ins")[0].split("g.")[1]
-        start_end = start_end.split("_")
-        if len(start_end) == 1:
-            start = int(start_end[0])
-            end = start
-        elif len(start_end) == 2:
-            start = int(start_end[0])
-            end = int(start_end[1])
-        else:
-            raise ValueError("Cannot decide start/end from {}.".format((hgvs, var_type, position)))
-    return start, end
-
-
 # convert one snp to json
 def _map_line_to_json(df):
     # specific variable treatment
     chrom = df["Chr"]
     if chrom == 'M':
         chrom = 'MT'
+
     position = int(df["Position"])
     ref, alt = df["Variant"].upper().split(">")
+
     HGVS, var_type = get_hgvs_from_vcf(chrom, position, ref, alt, mutant_type=True)
-    chromStart, chromEnd = get_pos_start_end_from_hgvs(HGVS, var_type, position)
 
     sampleid = df["SampleID"]
     studyname = df["StudyName"]
@@ -52,37 +34,31 @@ def _map_line_to_json(df):
     chrom = df["Chr"]
     position = df["Position"]
     variant = df["Variant"]
-    rsid = df["rsID"]
-    dbsnpbuild = df["DbsnpBuild"]
+    rsid = clean_rsid(df["rsID"], ("0", ))
+    dbsnpbuild = clean_data(df["DbsnpBuild"], ("0", ))
     ancestralallele = df["AncestralAllele"]
     kgenomecount = df["1000GenomeCount"]
     exacfreq = df["ExacFreq"]
     espaafreq = df["EspAaFreq"]
     espeafreq = df["EspEaFreq"]
-    transcript = df["Transcript"]
-    codingdnasize = df["codingDnaSize"]
-    gene = df["Gene"]
-    functionclass = df["FunctionClass"]
-    cdnavariant = df["cDnaVariant"]
-    proteinvariant = df["ProteinVariant"]
-    exon_intron = df["Exon_Intron"]
-    polyphen_hdiv = df["PolyPhen_HDiv"]
-    polyphen_hvar = df["PolyPhen_HVar"]
-    siftscore = df["SiftScore"]
-    caddscore = df["CaddScore"]
-    lofscore = df["LofScore"]
-    lrtscore = df["LrtScore"]
+    transcript = clean_data(df["Transcript"], ("none", ))
+    codingdnasize = clean_data(df["codingDnaSize"], ("-1", ))
+    gene = clean_data(df["Gene"], ("NA",))
+    functionclass = clean_data(df["FunctionClass"], ("none", ""))
+    cdnavariant = clean_data(df["cDnaVariant"], ("NA",))
+    proteinvariant = clean_data(df["ProteinVariant"], ("NA",))
+    exon_intron = clean_data(df["Exon_Intron"], ("NA",))
+    polyphen_hdiv = clean_data(df["PolyPhen_HDiv"], ("-1",))
+    polyphen_hvar = clean_data(df["PolyPhen_HVar"], ("-1",))
+    siftscore = clean_data(df["SiftScore"], ("-1",))
+    caddscore = clean_data(df["CaddScore"], ("-1",))
+    lofscore = clean_data(df["LofScore"], ("-1",))
+    lrtscore = clean_data(df["LrtScore"], ("-1",))
 
 # load as json data
     one_snp_json = {
         "_id": HGVS,
         "denovodb": {
-            "chrom": chrom,
-            "hg19": {
-                "start": chromStart,
-                "end": chromEnd
-            },
-
             "ref": ref,
             "alt": alt,
             "sampleid": sampleid,
@@ -93,7 +69,6 @@ def _map_line_to_json(df):
             "sequencetype":  sequencetype,
             "primaryphenotype": primaryphenotype,
             "validation": validation,
-            "chr": chrom,
             "position": position,
             "variant": variant,
             "rsid": rsid,
@@ -119,14 +94,26 @@ def _map_line_to_json(df):
         }
     }
 
-    one_snp_json = list_split(dict_sweep(unlist(value_convert_to_number(one_snp_json)), vals=[".", '-', None]), ";")
-    one_snp_json["denovodb"]["chrom"] = str(one_snp_json["denovodb"]["chrom"])
+    one_snp_json = list_split(dict_sweep(unlist(value_convert_to_number(one_snp_json)), vals=[None]), ";")
     return one_snp_json
 
 
 def clean_index(s):
     return s.replace("/", "_").replace("(", "_").replace(")", "").replace("#", "")
 
+
+def clean_data(d, vals):
+    if d in vals:
+        return None
+    else:
+        return d
+
+
+def clean_rsid(d, vals):
+    if d in vals:
+        return None
+    else:
+        return "rs{}".format(d)
 
 # open file, parse, pass to json mapper
 def load_data(input_file, version='hg19'):
@@ -137,10 +124,7 @@ def load_data(input_file, version='hg19'):
         index = next(db_denovodb)
     assert len(index) == VALID_COLUMN_NO, "Expecting %s columns, but got %s" % (VALID_COLUMN_NO, len(index))
     index = [clean_index(s) for s in index]
-    denovodb = []
-    for row in db_denovodb:
-        df = dict(zip(index, row))
-        denovodb.append(df)
+    denovodb = (dict(zip(index, row)) for row in db_denovodb)
     denovodb = filter(lambda row: row["Chr"] != "", denovodb)
     json_rows = map(_map_line_to_json, denovodb)
     json_rows = (row for row in json_rows if row)
