bf8cfbb465c4e533bf2ad3d0681904f8f66edfc4
Byung Ryul Jeon <labmed@gmail.com>
2017-08-08 15:29:08 -0700

    Changed dict_sweep function Bug fixed

--- src/dataload/sources/denovodb/denovodb_parser.py
+++ src/dataload/sources/denovodb/denovodb_parser.py
@@ -1,9 +1,10 @@
 import csv
-from biothings.utils.dataload import list_split, dict_sweep, unlist
+from biothings.utils.dataload import unlist
 from biothings.utils.dataload import value_convert_to_number
 from biothings.utils.dataload import merge_duplicate_rows
 from utils.hgvs import get_hgvs_from_vcf
 from itertools import groupby
+import numpy as np
 
 VALID_COLUMN_NO = 31
 
@@ -17,12 +18,9 @@ def _map_line_to_json(df):
     chrom = df["Chr"]
     if chrom == 'M':
         chrom = 'MT'
-
     position = int(df["Position"])
     ref, alt = df["Variant"].upper().split(">")
-
     HGVS, var_type = get_hgvs_from_vcf(chrom, position, ref, alt, mutant_type=True)
-
     sampleid = df["SampleID"]
     studyname = df["StudyName"]
     pubmedid = df["PubmedID"]
@@ -41,12 +39,12 @@ def _map_line_to_json(df):
     exacfreq = df["ExacFreq"]
     espaafreq = df["EspAaFreq"]
     espeafreq = df["EspEaFreq"]
-    transcript = clean_data(df["Transcript"], ("none", ))
+    transcript = clean_data(df["Transcript"], ("none", ""))
     codingdnasize = clean_data(df["codingDnaSize"], ("-1", ))
-    gene = clean_data(df["Gene"], ("NA",))
+    gene = clean_data(df["Gene"], ("NA", ""))
     functionclass = clean_data(df["FunctionClass"], ("none", ""))
-    cdnavariant = clean_data(df["cDnaVariant"], ("NA",))
-    proteinvariant = clean_data(df["ProteinVariant"], ("NA",))
+    cdnavariant = clean_data(df["cDnaVariant"], ("NA", ""))
+    proteinvariant = clean_data(df["ProteinVariant"], ("NA", ""))
     exon_intron = clean_data(df["Exon_Intron"], ("NA",))
     polyphen_hdiv = clean_data(df["PolyPhen_HDiv"], ("-1",))
     polyphen_hvar = clean_data(df["PolyPhen_HVar"], ("-1",))
@@ -93,8 +91,8 @@ def _map_line_to_json(df):
             "lrtscore":  lrtscore,
         }
     }
-
-    one_snp_json = list_split(dict_sweep(unlist(value_convert_to_number(one_snp_json)), vals=[None]), ";")
+    # one_snp_json = dict_sweep(unlist(value_convert_to_number(one_snp_json)), vals=[np.nan])
+    one_snp_json = value_convert_to_number(one_snp_json)
     return one_snp_json
 
 
@@ -104,16 +102,17 @@ def clean_index(s):
 
 def clean_data(d, vals):
     if d in vals:
-        return None
+        return np.nan
     else:
         return d
 
 
 def clean_rsid(d, vals):
     if d in vals:
-        return None
+        return np.nan
     else:
         return "rs{}".format(d)
+
 
 # open file, parse, pass to json mapper
 def load_data(input_file, version='hg19'):
@@ -128,5 +127,32 @@ def load_data(input_file, version='hg19'):
     denovodb = filter(lambda row: row["Chr"] != "", denovodb)
     json_rows = map(_map_line_to_json, denovodb)
     json_rows = (row for row in json_rows if row)
+    json_rows = sorted(json_rows, key=lambda row: row["_id"])
     row_groups = (it for (key, it) in groupby(json_rows, lambda row: row["_id"]))
-    return (merge_duplicate_rows(rg, "denovodb") for rg in row_groups)
+    json_rows = (merge_duplicate_rows(rg, "denovodb") for rg in row_groups)
+    return (unlist(dict_sweep(row, vals=[np.nan, ])) for row in json_rows)
+    # return (merge_duplicate_rows(rg, "denovodb") for rg in row_groups)
+
+
+def dict_sweep(d, vals=[".", "-", "", "NA", "none", " ", "Not Available", "unknown"]):
+    """
+    @param d: a dictionary
+    @param vals: a string or list of strings to sweep
+    """
+    for key, val in list(d.items()):
+        if val in vals:
+            del d[key]
+        elif isinstance(val, list):
+            val = [v for v in val if v not in vals]
+            for item in val:
+                if isinstance(item, dict):
+                    dict_sweep(item, vals)
+            if len(val) == 0:
+                del d[key]
+            else:
+                d[key] = val
+        elif isinstance(val, dict):
+            dict_sweep(val, vals)
+            if len(val) == 0:
+                del d[key]
+    return d
