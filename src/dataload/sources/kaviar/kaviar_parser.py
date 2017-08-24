4f970e5b357f572c880b52c4ef95de7bbd72786a
jeonbr <30668750+jeonbr@users.noreply.github.com>
2017-08-24 13:53:46 -0700

    change : Added Sorting

--- src/dataload/sources/kaviar/kaviar_parser.py
+++ src/dataload/sources/kaviar/kaviar_parser.py
@@ -1,39 +1,18 @@
 import vcf
 from biothings.utils.dataload import unlist
-from biothings.utils.dataload import value_convert_to_number, merge_duplicate_rows
+from biothings.utils.dataload import value_convert_to_number, merge_duplicate_rows, dict_sweep
 from utils.hgvs import get_hgvs_from_vcf
 from itertools import groupby, chain
-from collections import defaultdict
-import logging
-import os
+import os, csv
 
 VALID_COLUMN_NO = 8
-DATA_CHECK = True
 
-log_fn = './datacheck.log'
-if DATA_CHECK:
-    if os.path.isfile(log_fn):
-        os.remove(log_fn)
-
-logging.basicConfig(filename='./datacheck.log', level=logging.DEBUG)
-
-data_counter={
-    "ROWS": 0,
-    "NA7022,18" : 0,
-    "HGVS_Convert_ERROR" : 0,
-    "HGVS_ERROR_TYPE": defaultdict(int),
-    "DATA_INPUT" : 0,
-    "DATA_OUTPUT": 0,
-}
 '''this parser is for Kaviar version 160204-Public(All variants, annotated with data sources) downloaded from
 http://db.systemsbiology.net/kaviar/Kaviar.downloads.html'''
 
 
 # convert one snp to json
 def _map_line_to_json(item):
-    if DATA_CHECK:
-        data_counter['ROWS'] += 1
-
     chrom = item.CHROM
     chromStart = item.POS
     ref = item.REF
@@ -59,8 +38,6 @@ def _map_line_to_json(item):
     # convert vcf object to string
     item.ALT = [str(alt) for alt in item.ALT]
 
-
-
     # if multiallelic, put all variants as a list in multi-allelic field
     hgvs_list = None
     if len(item.ALT) > 1:
@@ -80,26 +57,17 @@ def _map_line_to_json(item):
                 ds_list = ds_str.split(",")
                 info['DS'] = [d.replace("NA7022_18", "NA7022,18") for d in ds_list]
                 assert len(item.ALT) ==len(info['DS']), "info.DS mismatch, %s: %s\n## DS: %s" % (item, info['DS'])
-                if DATA_CHECK:
-                    data_counter["NA7022,18"] += 1
-                    logging.debug("DS: %s" % (item))
 
     for i, alt in enumerate(item.ALT):
-        if DATA_CHECK:
-            data_counter["DATA_INPUT"] += 1
         try:
             (HGVS, var_type) = get_hgvs_from_vcf(chrom, chromStart, ref, alt, mutant_type=True)
         except:
-            if DATA_CHECK:
-                data_counter["HGVS_Convert_ERROR"] += 1
-                data_counter["HGVS_ERROR_TYPE"][alt] += 1
-                logging.debug("HGVS_Convert_ERROR: %s" % (item))
             continue
 
         if HGVS is None:
             return
-        # load as json data
 
+        # load as json data
         one_snp_json = {
             "_id": HGVS,
             "kaviar": {
@@ -113,10 +81,7 @@ def _map_line_to_json(item):
             }
         }
 
-        one_snp_json = value_convert_to_number(one_snp_json)
-        if DATA_CHECK:
-            data_counter["DATA_OUTPUT"] += 1
-        yield one_snp_json
+        yield value_convert_to_number(one_snp_json)
 
 
 # open file, parse, pass to json mapper
@@ -124,10 +89,26 @@ def load_data(input_file):
     vcf_reader = vcf.Reader(open(input_file, 'r'), strict_whitespace=True)
     json_rows = map(_map_line_to_json, vcf_reader)
     json_rows = chain.from_iterable(json_rows)
-    # It is assumed that json_row is already sorted.
+
+    if not os.path.exists("alldata.csv"):
+        print("Writing data")
+        with open("alldata.csv", "w") as f:
+            dbwriter = csv.writer(f)
+            for doc in json_rows:
+                dbwriter.writerow([doc['_id'], str(doc)])
+    if not os.path.exists("sorted.csv"):
+        print("Start Sorting")
+        import subprocess
+        p = subprocess.Popen('sort alldata.csv > sorted.csv', shell=True)
+        os.waitpid(p.pid, 0)
+        print("Sorted")
+
+    json_rows = csv.reader(open('sorted.csv'))
+    json_rows = (eval(row[1]) for row in json_rows)
     row_groups = (it for (key, it) in groupby(json_rows, lambda row: row["_id"]))
     json_rows = (merge_duplicate_rows(rg, "kaviar") for rg in row_groups)
     return (unlist(dict_sweep(row, vals=[None, ])) for row in json_rows)
+
 
 def dict_sweep(d, vals=[".", "-", "", "NA", "none", " ", "Not Available", "unknown"]):
     """
