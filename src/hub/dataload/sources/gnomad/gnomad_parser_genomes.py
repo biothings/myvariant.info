39b3b1d38b6d79af5e54047434ca821c9f699f7f
kevinxin90 <kevinxin@scripps.edu>
2017-09-14 11:06:58 -0700

    remove r from vcf reader

--- src/hub/dataload/sources/gnomad/gnomad_parser_genomes.py
+++ src/hub/dataload/sources/gnomad/gnomad_parser_genomes.py
@@ -78,7 +78,7 @@ def _map_line_to_json(item, keys):
         yield obj
 
 def load_data(input_file):
-    vcf_reader = vcf.Reader(filename=input_file, 'r')
+    vcf_reader = vcf.Reader(filename=input_file)
     keys = vcf_reader.infos.keys()
     keys = [_key for _key in keys if _key.startswith(("AC", "AF", "AN", "Hom", "GC", "Hemi"))]
     for record in vcf_reader:
