d93541d9a53439abdf42d2f2ad0058d18eef7a30
Sebastien Lelong <slelong@scripps.edu>
2017-12-21 12:14:08 -0800

    VERIFY_CERT part of base http dumper

--- src/hub/dataload/sources/cgi/cgi_dump.py
+++ src/hub/dataload/sources/cgi/cgi_dump.py
@@ -16,12 +16,9 @@ class CGIDumper(LastModifiedHTTPDumper):
     SRC_NAME = "cgi"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = "0 9 * * *"
+    VERIFY_CERT = False
     # URL is always the same, but headers change
     SRC_URLS = ["https://www.cancergenomeinterpreter.org/data/cgi_biomarkers_latest.zip"]
-
-    def prepare_client(self):
-        self.client = requests.Session()
-        self.client.verify = False
 
     def post_dump(self, *args, **kwargs):
         self.logger.info("Unzipping files in '%s'" % self.new_data_folder) 
