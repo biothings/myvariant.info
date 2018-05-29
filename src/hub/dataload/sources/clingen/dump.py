b13d072c35cd37bf9d043ccf89a2532e41e7154f
Sebastien Lelong <slelong@scripps.edu>
2018-05-29 11:14:55 -0700

    parallelized clingen uploader (post-dump step splits input file)

--- src/hub/dataload/sources/clingen/dump.py
+++ src/hub/dataload/sources/clingen/dump.py
@@ -3,6 +3,7 @@ import os.path
 import sys
 import time
 import requests
+import subprocess
 
 import biothings, config
 biothings.config_for_app(config)
@@ -17,8 +18,15 @@ class ClingenDumper(LastModifiedHTTPDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = "0 9 * * *"
     SRC_URLS = ["http://reg.genome.network/temp/mvi_ca.gz"]
+    CHUNK_SIZE = 10000000 # number of lines
+
 
     def post_dump(self, *args, **kwargs):
         self.logger.info("Unzipping files in '%s'" % self.new_data_folder) 
-        gunzipall(self.new_data_folder)
+        #gunzipall(self.new_data_folder)
+        input_file = os.path.join(self.new_data_folder,"mvi_ca")
+        self.logger.info("Split file in chunks")
+        subprocess.check_call(["split","-l","%s" % self.__class__.CHUNK_SIZE,input_file,"%s.split." % input_file])
+
+
 
--- src/hub/dataload/sources/clingen/upload.py
+++ src/hub/dataload/sources/clingen/upload.py
@@ -1,24 +1,32 @@
-import os
+import os, glob
+
+import biothings.hub.dataload.uploader as uploader
 
 from ...uploader import SnpeffPostUpdateUploader
 from .parser import load_data
 
 
-class ClingenUploader(SnpeffPostUpdateUploader):
+class ClingenUploader(SnpeffPostUpdateUploader,uploader.ParallelizedSourceUploader):
     name = "clingen"
     __metadata__ = {"mapper": 'observed',
-                    "assembly": "hg38",
-                    "src_meta": {
-                        "url": "https://www.clinicalgenome.org",
-                        "license_url": "https://www.clinicalgenome.org/about/terms-of-use",
-                        "license_url_short": "",
-                        "licence": "CC0 1.0",
-                    }
-                    }
+            "assembly": "hg38",
+            "src_meta": {
+                "url": "https://www.clinicalgenome.org",
+                "license_url": "https://www.clinicalgenome.org/about/terms-of-use",
+                "license_url_short": "",
+                "licence": "CC0 1.0",
+                }
+            }
 
-    def load_data(self,data_folder):
+    GLOB_PATTERN = "mvi_ca.split.*"
+
+    def jobs(self):
+        # tuple(input_file,) (only one arg, but still need a tuple
+        return map(lambda e: (e, ),
+                   glob.glob(os.path.join(self.data_folder, self.__class__.GLOB_PATTERN)))
+
+    def load_data(self,input_file):
         # there's one vcf file there, let's get it
-        input_file = os.path.join(data_folder,"mvi_ca")
         assert os.path.exists(input_file), "Can't find input file '%s'" % input_file
         self.logger.info("Load data from file '%s'" % input_file)
         return load_data(input_file)
