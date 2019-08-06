aa8e6e7f3ee9d2a3d49f258e5171c5365e512274
Jerry <xzhou@scripps.edu>
2019-08-06 10:51:18 -0700

    Update testing class

--- src/tests/local.py
+++ src/tests/local.py
@@ -3,19 +3,17 @@
 '''
 
 from nose.core import run
-from tornado.web import Application
 
 from biothings.tests import TornadoTestServerMixin
 from tests.remote import MyVariantRemoteTest
 from web.settings import MyVariantWebSettings
 
 
-class MyChemTestTornadoClient(TornadoTestServerMixin, MyVariantRemoteTest):
+class MyVariantLocalTest(TornadoTestServerMixin, MyVariantRemoteTest):
     '''
         Self contained test class, can be used for CI tools such as Travis
         Starts a Tornado server on its own and perform tests against this server.
     '''
-    __test__ = True
 
     # Override default setting loader
     settings = MyVariantWebSettings(config='config')
@@ -24,6 +22,6 @@ class MyChemTestTornadoClient(TornadoTestServerMixin, MyVariantRemoteTest):
 if __name__ == '__main__':
     print()
     print('MyVariant Local Test')
-    print('-'*70)
+    print('-' * 70)
     print()
     run(argv=['', '--logging-level=INFO', '-v'], defaultTest='__main__.MyVariantLocalTest')
