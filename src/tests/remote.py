5fe11716e33f6b1f121e8555cc34a534b7ee4423
Sebastien Lelong <sebastien.lelong@gmail.com>
2020-01-02 16:00:28 +0000

    test to check counts

--- src/tests/remote.py
+++ src/tests/remote.py
@@ -5,6 +5,7 @@ import os
 
 from nose.core import runmodule
 from nose.tools import eq_, ok_
+from pprint import pformat
 
 from biothings.tests import BiothingsTestCase
 
@@ -329,6 +330,38 @@ class MyVariantRemoteTest(BiothingsTestCase):
         assert "_seqhashed_" in res["hits"][0]["_id"]
         h = res["hits"][0]["_id"].split("_seqhashed_")[-1]
         assert h in res["hits"][0]["_seqhashed"]
+
+    def check_index_count(self, assembly):
+        meta = self.request("metadata?assembly=%s" % assembly).json()
+        results = {}
+        for src_name in meta["src"]:
+            if src_name == "snpeff":
+                continue # not a root src, counts always different
+                         # TODO: that said, count in meta could be correct...
+            stats = meta["src"][src_name]["stats"]
+            for stat in stats:
+                subsrc = stat.replace("_%s" % assembly,"")
+                if subsrc in ("gnomad_genomes","gnomad_exomes"):
+                    subsrc = subsrc.rstrip("s") # plural in meta, singular in docs
+                meta_cnt = meta["src"][src_name]["stats"][stat]
+                res = self.request("query?q=_exists_:%s&size=0&assembly=%s" % (subsrc,assembly)).json()
+                results[subsrc] = {"meta" : meta_cnt, "index" : res["total"]}
+            #assert res["total"] == meta_cnt, "Count in metadata (%s) doesn't match count from query (%s) for datasource '%s'" % (meta_cnt,res["total"],subsrc)
+        errs = {}
+        for src in results:
+            mc = results[src]["meta"]
+            ic = results[src]["index"]
+            if mc != ic:
+                errs[src] = results[src]
+                errs[src]["diff"] = mc - ic
+        assert len(errs) == 0, "Some counts don't match metadata:\n%s" % pformat(errs)
+
+
+    def test_index_count_hg19(self):
+        self.check_index_count("hg19")
+
+    def test_index_count_hg38(self):
+        self.check_index_count("hg38")
 
 
 if __name__ == '__main__':
