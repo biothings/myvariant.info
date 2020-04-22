ea51f135f248961cd4f7e79c7ee17b3ac8c017f4
Jerry <xzhou@scripps.edu>
2020-04-21 19:07:02 -0700

    Update beacon handlers

--- src/index.py
+++ src/index.py
@@ -1,13 +1,13 @@
 from tornado.web import StaticFileHandler, RedirectHandler
 
 from biothings.web.index_base import main
-# from web.beacon.handlers import BeaconHandler, BeaconInfoHandler
+from web.beacon.handlers import BeaconHandler, BeaconInfoHandler
 
 if __name__ == "__main__":
     main([
         (r"/", RedirectHandler, {"url": "/standalone"}), # override default frontpage
         (r"/demo/?()", StaticFileHandler, {"path": "docs/demo", "default_filename": "index.html"}),
         (r"/standalone/?()", StaticFileHandler, {"path": "docs/standalone", "default_filename": "index.html"}),
-        # (r"/beacon/query?", BeaconHandler),
-        # (r"/beacon/info", BeaconInfoHandler),
+        (r"/beacon/query?", BeaconHandler),
+        (r"/beacon/info", BeaconInfoHandler),
     ])
