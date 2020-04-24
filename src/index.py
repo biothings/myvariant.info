28ba4b81db18c15bb230ca544acb2e81e64cb278
Jerry <xzhou@scripps.edu>
2020-04-23 21:44:52 -0700

    Prevent localhost redirection cache

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ from web.beacon.handlers import BeaconHandler, BeaconInfoHandler
 
 if __name__ == "__main__":
     main([
-        (r"/", RedirectHandler, {"url": "/standalone"}), # override default frontpage
+        (r"/", RedirectHandler, {"url": "/standalone", "permanent": False}),  # override default frontpage
         (r"/demo/?()", StaticFileHandler, {"path": "docs/demo", "default_filename": "index.html"}),
         (r"/standalone/?()", StaticFileHandler, {"path": "docs/standalone", "default_filename": "index.html"}),
         (r"/beacon/query?", BeaconHandler),
