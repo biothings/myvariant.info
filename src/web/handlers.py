ed59c7c3798b89475869222e4b2edf379f4d78c4
Jerry <xzhou@scripps.edu>
2020-08-16 17:08:39 -0700

    Add metadata assembly switch

---
+++ src/web/handlers.py
@@ -0,0 +1,19 @@
+from tornado.web import RequestHandler
+
+from biothings.web.handlers.es import (MetadataFieldHandler,
+                                       MetadataSourceHandler)
+
+
+class AssemblyAwareMixin(RequestHandler):
+    
+    def prepare(self):
+        super().prepare()
+        self.biothing_type = self.args.es.assembly
+
+class MVMetadataFieldHandler(AssemblyAwareMixin, MetadataFieldHandler):
+
+    pass
+
+class MVMetadataSourceHandler(AssemblyAwareMixin, MetadataSourceHandler):
+
+    pass
