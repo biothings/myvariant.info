01cf1e7a81cc024717962e16451b9f3cbd3bd1a6
Jerry <xzhou@scripps.edu>
2021-08-08 15:59:56 -0700

    0.10.x indexer upgrade

--- src/hub/dataindex/indexer.py
+++ src/hub/dataindex/indexer.py
@@ -1,56 +1,64 @@
-import os, time
+import time
 import asyncio
 
 import config
-import biothings.hub.dataindex.indexer as indexer
+from biothings.hub.dataindex.indexer import Indexer, IndexManager, ColdHotIndexer
 from biothings.hub.dataexport.ids import export_ids, upload_ids
 from biothings.utils.hub_db import get_src_build
 from biothings.utils.es import ESIndexer
 from utils.stats import update_stats
 
 
-class BaseVariantIndexer(indexer.Indexer):
-    
-    def enrich_final_mapping(self,final_mapping):
-        # enrich with myvariant specific stuff
-        final_mapping["properties"]["chrom"] = {
-                'analyzer': 'string_lowercase',
-                'type': 'text'}
-        final_mapping["properties"]["observed"] = {
-                "type": "boolean"}
-        final_mapping["properties"]["_seqhashed"] = {
-                "type" : "object",
-                "properties" : {
-                    "_flag": {
-                        "type" : "boolean"
-                        }
-                    }
+class BaseVariantIndexer(Indexer):
+
+    def __init__(self, build_doc, indexer_env, target_name, index_name):
+        super().__init__(build_doc, indexer_env, target_name, index_name)
+
+        self.es_index_mappings["properties"]["chrom"] = {
+            'analyzer': 'string_lowercase',
+            'type': 'text'
+        }
+        self.es_index_mappings["properties"]["observed"] = {
+            "type": "boolean"
+        }
+        self.es_index_mappings["properties"]["_seqhashed"] = {
+            "type": "object",
+            "properties": {
+                "_flag": {
+                    "type": "boolean"
                 }
+            }
+        }
+        self.es_index_settings["mapping"] = {
+            "total_fields": {
+                "limit": 2000
+            }
+        }
+        self.assembly = build_doc["build_config"]["assembly"]
 
-        return final_mapping
-
-    def get_index_creation_settings(self):
-        settings = super(BaseVariantIndexer,self).get_index_creation_settings()
-        settings.setdefault("mapping",{}).setdefault("total_fields",{})["limit"] = 2000
-        return settings
-
-    def post_index(self, target_name, index_name, job_manager, steps=["index","post"], batch_size=10000, ids=None, mode=None): 
-        # TODO: not tested yet
+    @asyncio.coroutine
+    def post_index(self):
+        # Migrated from Sebastian's commit 1a7b7a
+        # It was orginally marked "Not Tested Yet".
         self.logger.info("Sleeping for a bit while index is being fully updated...")
-        time.sleep(3*60)
-        idxer = ESIndexer(index=index_name,doc_type=self.doc_type,es_host=self.host)
-        self.logger.info("Updating 'stats' by querying index '%s'" % index_name)
-        assembly = self.build_config["assembly"]
-        return update_stats(idxer,assembly)
+        yield from time.sleep(3*60)
+        idxer = ESIndexer(
+            index=self.es_index_name,
+            doc_type=self.doc_type,
+            es_host=self.es_client_args.get('hosts'))
+        self.logger.info("Updating 'stats' by querying index '%s'" % self.es_index_name)
+        return update_stats(idxer, self.assembly)
 
 
-class MyVariantIndexerManager(indexer.IndexManager):
+class MyVariantIndexerManager(IndexManager):
+
+    # New Hub Command
 
     def post_publish(self, snapshot, index, *args, **kwargs):
         # assuming build name == index name, and assuming demo index has
         # "demo" in its name...
         # assuming full index, not demo, guess name now
-        bdoc = get_src_build().find_one({"_id" : index})
+        bdoc = get_src_build().find_one({"_id": index})
         assert bdoc, "Can't find build doc associated with index '%s' (should be named the same)" % index
         ids_file = export_ids(index)
         if "hg19" in index or "hg19" in snapshot:
@@ -59,15 +67,15 @@ class MyVariantIndexerManager(indexer.IndexManager):
             redir = "hg38_ids.xz"
         if "demo" in index or "demo" in snapshot:
             redir = "demo_%s" % redir
-        upload_ids(ids_file, redir, 
-                s3_bucket=config.IDS_S3_BUCKET,
-                aws_key=config.AWS_KEY,
-                aws_secret=config.AWS_SECRET)
+        upload_ids(ids_file, redir,
+                   s3_bucket=config.IDS_S3_BUCKET,
+                   aws_key=config.AWS_KEY,
+                   aws_secret=config.AWS_SECRET)
+
 
 class VariantIndexer(BaseVariantIndexer):
     pass
 
-class ColdHotVariantIndexer(indexer.ColdHotIndexer,BaseVariantIndexer):
+
+class ColdHotVariantIndexer(ColdHotIndexer, BaseVariantIndexer):
     pass
-    
-    
