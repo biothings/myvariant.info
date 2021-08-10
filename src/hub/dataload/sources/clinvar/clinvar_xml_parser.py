24e3a9ccfb746e59edbc892b29412e3f34eaeee3
Yao Yao <yaoy.cs@gmail.com>
2021-08-10 15:37:55 -0700

    formatted code; renamed variables and functions

--- src/hub/dataload/sources/clinvar/clinvar_xml_parser.py
+++ src/hub/dataload/sources/clinvar/clinvar_xml_parser.py
@@ -1,104 +1,89 @@
-# a python lib is generated on the fly, in data folder
-# adjust path
-
-
-import sys, os, glob
+import glob
+import os
+import sys
 from itertools import groupby
 
-from config import DATA_ARCHIVE_ROOT, logger as logging
-import biothings, config
+import biothings
+import config
 biothings.config_for_app(config)
-from biothings.utils.dataload import unlist, dict_sweep, \
-                                     value_convert_to_number, rec_handler
+
+from biothings.utils.dataload import unlist, dict_sweep, value_convert_to_number, rec_handler
 
 GLOB_PATTERN = "ClinVarFullRelease_*.xml.gz"
 clinvarlib = None
 
+
 def import_clinvar_lib(data_folder):
-    sys.path.insert(0,data_folder)
+    # a python lib is generated on the fly, in data folder
+    sys.path.insert(0, data_folder)
     import genclinvar as clinvar_mod
     global clinvarlib
     clinvarlib = clinvar_mod
 
-def merge_rcv_accession(generator):
-    groups = []
-    for key, group in groupby(generator, lambda x: x['_id']):
-        groups.append(list(group))
+
+def merge_rcv_accession(records):
+    record_groups = []
+    for key, group in groupby(records, lambda x: x['_id']):
+        record_groups.append(list(group))
 
     # get the number of groups, and uniquekeys
-    logging.info("number of groups: %s" % len(groups))
+    logging.info("number of groups: %s" % len(record_groups))
 
-    # loop through each item, if item number >1, merge rcv accession number
-    for item in groups:
-        rcv_new = []
-        if len(item) > 1:
-            json_item = item[0]
-            for _item in item:
-                rcv_info = _item['clinvar']['rcv']
-                rcv_new.append(rcv_info)
-            json_item['clinvar']['rcv'] = rcv_new
-            yield json_item
+    # Each group of records is a list
+    # Loop through each group, if record number >1, merge rcv accessions
+    for record_list in record_groups:
+        if len(record_list) == 1:
+            yield record_list[0]
         else:
-            yield item[0]
+            rcv_list = [record['clinvar']['rcv'] for record in record_list]
+            
+            merged_record = record_list[0]
+            merged_record['clinvar']['rcv'] = rcv_list
+            yield merged_record
 
 
 def parse_measure(Measure, hg19=True):
+    # exclude any item of which types belong to 'Variation', 'protein only' or 'Microsatellite'
     variation_type = Measure.Type
-    # exclude any item of which types belong to
-    # 'Variation', 'protein only' or 'Microsatellite'
     if variation_type == 'Variation' or variation_type == 'protein only' or variation_type == 'Microsatellite':
         return None
+
     allele_id = Measure.ID
+
     chrom = None
-    chromStart_19 = None
-    chromEnd_19 = None
-    chromStart_38 = None
-    chromEnd_38 = None
-    ref = None
-    alt = None
+    chromStart_19, chromEnd_19, chromStart_38, chromEnd_38 = None, None, None, None
+    ref, alt = None, None
     if Measure.SequenceLocation:
         for SequenceLocation in Measure.SequenceLocation:
-            # In this version, only accept information concerning GRCh37
             if 'GRCh37' in SequenceLocation.Assembly:
                 chrom = SequenceLocation.Chr
                 chromStart_19 = SequenceLocation.start
                 chromEnd_19 = SequenceLocation.stop
                 if not ref:
-                    ref = SequenceLocation.referenceAllele
-                if not ref:
-                    ref = SequenceLocation.referenceAlleleVCF
+                    ref = SequenceLocation.referenceAllele or SequenceLocation.referenceAlleleVCF
                 if not alt:
-                    alt = SequenceLocation.alternateAllele
-                if not alt:
-                    alt = SequenceLocation.alternateAlleleVCF
+                    alt = SequenceLocation.alternateAllele or SequenceLocation.alternateAlleleVCF
 
             if 'GRCh38' in SequenceLocation.Assembly:
                 chromStart_38 = SequenceLocation.start
                 chromEnd_38 = SequenceLocation.stop
                 if not ref:
-                    ref = SequenceLocation.referenceAllele
-                if not ref:
-                    ref = SequenceLocation.referenceAlleleVCF
+                    ref = SequenceLocation.referenceAllele or SequenceLocation.referenceAlleleVCF
                 if not alt:
-                    alt = SequenceLocation.alternateAllele
-                if not alt:
-                    alt = SequenceLocation.alternateAlleleVCF
+                    alt = SequenceLocation.alternateAllele or SequenceLocation.alternateAlleleVCF
 
+    symbol = None
+    gene_id = None
     if Measure.MeasureRelationship:
         try:
-            symbol = Measure.MeasureRelationship[0].\
-                Symbol[0].get_ElementValue().valueOf_
+            symbol = Measure.MeasureRelationship[0].Symbol[0].get_ElementValue().valueOf_
         except:
             symbol = None
         gene_id = Measure.MeasureRelationship[0].XRef[0].ID
-    else:
-        symbol = None
-        gene_id = None
 
+    name = None
     if Measure.Name:
         name = Measure.Name[0].ElementValue.valueOf_
-    else:
-        name = None
 
     if len(Measure.CytogeneticLocation) == 1:
         cytogenic = Measure.CytogeneticLocation[0]
@@ -126,6 +111,7 @@ def parse_measure(Measure, hg19=True):
                 if 'HGVS, genomic, top level' in AttributeSet.Attribute.Type:
                     if AttributeSet.Attribute.integerValue == 37:
                         hgvs_genome = AttributeSet.Attribute.get_valueOf_()
+
                 if 'genomic' in AttributeSet.Attribute.Type:
                     HGVS['genomic'].append(AttributeSet.Attribute.get_valueOf_())
                 elif 'non-coding' in AttributeSet.Attribute.Type:
@@ -144,11 +130,11 @@ def parse_measure(Measure, hg19=True):
                     HGVS['coding'].append(AttributeSet.Attribute.get_valueOf_())
                 elif 'protein' in AttributeSet.Attribute.Type:
                     HGVS['protein'].append(AttributeSet.Attribute.get_valueOf_())
-                if AttributeSet.Attribute.Type == 'HGVS, coding, RefSeq':
+
+                if not hgvs_coding and AttributeSet.Attribute.Type == 'HGVS, coding, RefSeq':
                     hgvs_coding = AttributeSet.Attribute.get_valueOf_()
-                elif AttributeSet.Attribute.Type == 'HGVS, genomic, top level, previous':
+                if not hgvs_genome and AttributeSet.Attribute.Type == 'HGVS, genomic, top level, previous':
                     hgvs_genome = AttributeSet.Attribute.get_valueOf_()
-                    break
 
         if chrom and chromStart and chromEnd:
             # if its SNP, make sure chrom, chromStart, chromEnd, ref, alt are all provided
@@ -156,12 +142,13 @@ def parse_measure(Measure, hg19=True):
                 if ref and alt:
                     hgvs_id = "chr%s:g.%s%s>%s" % (chrom, chromStart, ref, alt)
                 else:
-                    print('hgvs not found chr {}, chromStart {}, chromEnd {}, ref {}, alt {}, allele id {}'.format(chrom, chromStart, chromEnd, ref, alt, allele_id))
+                    print('hgvs not found chr {}, chromStart {}, chromEnd {}, ref {}, alt {}, allele id {}'.
+                          format(chrom, chromStart, chromEnd, ref, alt, allele_id))
             # items whose type belong to 'Indel, Insertion, \
             # Duplication' might not hava explicit alt information, \
             # so we will parse from hgvs_genome
             elif variation_type == 'Indel':
-            # RCV000156073, NC_000010.10:g.112581638_112581639delinsG
+                # RCV000156073, NC_000010.10:g.112581638_112581639delinsG
                 if hgvs_genome:
                     indel_position = hgvs_genome.find('del')
                     indel_alt = hgvs_genome[indel_position+3:]
@@ -227,68 +214,69 @@ def parse_measure(Measure, hg19=True):
     if hgvs_id:
         one_snp_json = {
             "_id": hgvs_id,
-            "clinvar":
-                {
-                    "allele_id": allele_id,
-                    "chrom": chrom,
-                    "omim": omim,
-                    "cosmic": cosmic,
-                    "uniprot": uniprot,
-                    "dbvar": dbvar,
-                    "hg19": {
-                        "start": chromStart_19,
-                        "end": chromEnd_19
-                    },
-                    "hg38": {
-                        "start": chromStart_38,
-                        "end": chromEnd_38
-                    },
-                    "type": variation_type,
-                    "gene": {
-                        "id": gene_id,
-                        "symbol": symbol
-                    },
-                    "rcv": {
-                        "preferred_name": name,
-                    },
-                    "rsid": rsid,
-                    "cytogenic": cytogenic,
-                    "hgvs": HGVS,
-                    "coding_hgvs_only": coding_hgvs_only,
-                    "ref": ref,
-                    "alt": alt
-                }
+            "clinvar": {
+                "allele_id": allele_id,
+                "chrom": chrom,
+                "omim": omim,
+                "cosmic": cosmic,
+                "uniprot": uniprot,
+                "dbvar": dbvar,
+                "hg19": {
+                    "start": chromStart_19,
+                    "end": chromEnd_19
+                },
+                "hg38": {
+                    "start": chromStart_38,
+                    "end": chromEnd_38
+                },
+                "type": variation_type,
+                "gene": {
+                    "id": gene_id,
+                    "symbol": symbol
+                },
+                "rcv": {
+                    "preferred_name": name,
+                },
+                "rsid": rsid,
+                "cytogenic": cytogenic,
+                "hgvs": HGVS,
+                "coding_hgvs_only": coding_hgvs_only,
+                "ref": ref,
+                "alt": alt
+            }
         }
 
         return one_snp_json
 
-def _map_line_to_json(cp, hg19):
+
+def _map_parsed_record_to_json(parsed_record, hg19):
     try:
-        clinical_significance = cp.ReferenceClinVarAssertion.ClinicalSignificance.Description
+        clinical_significance = parsed_record.ReferenceClinVarAssertion.ClinicalSignificance.Description
     except:
         clinical_significance = None
 
-    rcv_accession = cp.ReferenceClinVarAssertion.ClinVarAccession.Acc
+    rcv_accession = parsed_record.ReferenceClinVarAssertion.ClinVarAccession.Acc
 
     try:
-        review_status = cp.ReferenceClinVarAssertion.ClinicalSignificance.ReviewStatus
+        review_status = parsed_record.ReferenceClinVarAssertion.ClinicalSignificance.ReviewStatus
     except:
         review_status = None
 
     try:
-        last_evaluated = cp.ReferenceClinVarAssertion.ClinicalSignificance.DateLastEvaluated
+        last_evaluated = parsed_record.ReferenceClinVarAssertion.ClinicalSignificance.DateLastEvaluated
     except:
         last_evaluated = None
     
-    number_submitters = len(cp.ClinVarAssertion)
+    number_submitters = len(parsed_record.ClinVarAssertion)
+
     # some items in clinvar_xml doesn't have origin information
     try:
-        origin = cp.ReferenceClinVarAssertion.ObservedIn[0].Sample.Origin
+        origin = parsed_record.ReferenceClinVarAssertion.ObservedIn[0].Sample.Origin
     except:
         origin = None
 
     conditions = []
-    for _trait in cp.ReferenceClinVarAssertion.TraitSet.Trait:
+    for _trait in parsed_record.ReferenceClinVarAssertion.TraitSet.Trait:
         synonyms = []
         conditions_name = ''
         for name in _trait.Name:
@@ -313,32 +301,35 @@ def _map_line_to_json(cp, hg19):
             if _set.Attribute.Type == 'age of onset':
                 age_of_onset = _set.Attribute.get_valueOf_()
 
-        conditions.append({"name": conditions_name, "synonyms": synonyms, "identifiers": identifiers, "age_of_onset": age_of_onset})
+        conditions.append({"name": conditions_name, "synonyms": synonyms, "identifiers": identifiers,
+                           "age_of_onset": age_of_onset})
 
     try:
-        genotypeset = cp.ReferenceClinVarAssertion.GenotypeSet
+        genotypeset = parsed_record.ReferenceClinVarAssertion.GenotypeSet
     except:
         genotypeset = None
 
     if genotypeset:
         obj_list = []
         id_list = []
-        for _set in cp.ReferenceClinVarAssertion.GenotypeSet.MeasureSet:
+        for _set in parsed_record.ReferenceClinVarAssertion.GenotypeSet.MeasureSet:
             variant_id = _set.ID
             for _measure in _set.Measure:
                 json_obj = parse_measure(_measure, hg19=hg19)
                 if json_obj:
-                    json_obj['clinvar']['rcv'].update({'accession': rcv_accession,
+                    json_obj['clinvar']['rcv'].update({
+                        'accession': rcv_accession,
                         'clinical_significance': clinical_significance,
                         'number_submitters': number_submitters,
                         'review_status': review_status,
                         'last_evaluated': str(last_evaluated),
                         'origin': origin,
-                        'conditions': conditions})
+                        'conditions': conditions
+                    })
                     json_obj['clinvar'].update({'variant_id': variant_id})
                     json_obj = (dict_sweep(unlist(value_convert_to_number(json_obj,
-                                               ['chrom', 'omim', 'id', 'orphanet', 'gene',
-                                                'rettbase_(cdkl5)', 'cosmic', 'dbrbc'])), [None, '', 'None']))
+                                ['chrom', 'omim', 'id', 'orphanet', 'gene', 'rettbase_(cdkl5)', 'cosmic', 'dbrbc'])),
+                                [None, '', 'None']))
                     obj_list.append(json_obj)
                     id_list.append(json_obj['_id'])
         for _obj in obj_list:
@@ -348,17 +339,19 @@ def _map_line_to_json(cp, hg19):
             }})
             yield _obj
     else:
-        variant_id = cp.ReferenceClinVarAssertion.MeasureSet.ID
-        for _measure in cp.ReferenceClinVarAssertion.MeasureSet.Measure:
+        variant_id = parsed_record.ReferenceClinVarAssertion.MeasureSet.ID
+        for _measure in parsed_record.ReferenceClinVarAssertion.MeasureSet.Measure:
             json_obj = parse_measure(_measure, hg19=hg19)
             if json_obj:
-                json_obj['clinvar']['rcv'].update({'accession': rcv_accession,
-                        'clinical_significance': clinical_significance,
-                        'number_submitters': number_submitters,
-                        'review_status': review_status,
-                        'last_evaluated': str(last_evaluated),
-                        'origin': origin,
-                        'conditions': conditions})
+                json_obj['clinvar']['rcv'].update({
+                    'accession': rcv_accession,
+                    'clinical_significance': clinical_significance,
+                    'number_submitters': number_submitters,
+                    'review_status': review_status,
+                    'last_evaluated': str(last_evaluated),
+                    'origin': origin,
+                    'conditions': conditions
+                })
                 json_obj['clinvar'].update({'variant_id': variant_id})
                 json_obj = (dict_sweep(unlist(value_convert_to_number(json_obj,
                                                ['chrom', 'omim', 'id', 'orphanet', 'gene',
@@ -366,23 +359,23 @@ def _map_line_to_json(cp, hg19):
                 yield json_obj
 
 
-def rcv_feeder(input_file, hg19):
+def clinvar_json_record_feeder(input_file, hg19):
     # the first two line of clinvar_xml is not useful information
-    cv_data = rec_handler(input_file, block_end='</ClinVarSet>\n',
-                          skip=2, include_block_end=True)
-    for record in cv_data:
+    xml_records = rec_handler(input_file, block_end='</ClinVarSet>\n', skip=2, include_block_end=True)
+    for xml_record in xml_records:
         # some exceptions
-        if record.startswith('\n</ReleaseSet>'):
+        if xml_record.startswith('\n</ReleaseSet>'):
             continue
 
         try:
-            record_parsed = clinvarlib.parseString(record, silence=1)
+            parsed_record = clinvarlib.parseString(xml_record, silence=1)
         except:
-            logging.debug(record)
+            logging.debug(xml_record)
             raise
 
-        for record_mapped in _map_line_to_json(record_parsed, hg19):
-            yield record_mapped
+        for json_record in _map_parsed_record_to_json(parsed_record, hg19):
+            yield json_record
+
 
 def load_data(data_folder, version):
     # try to get logger from uploader
@@ -391,18 +384,23 @@ def load_data(data_folder, version):
     logging = loggingmod.getLogger("clinvar_upload")
 
     import_clinvar_lib(data_folder)
-    files = glob.glob(os.path.join(data_folder,GLOB_PATTERN))
-    assert len(files) == 1, "Expecting only one file matching '%s', got: %s" % (GLOB_PATTERN,files)
+
+    files = glob.glob(os.path.join(data_folder, GLOB_PATTERN))
+    assert len(files) == 1, "Expecting only one file matching '%s', got: %s" % (GLOB_PATTERN, files)
     input_file = files[0]
-    data_generator = rcv_feeder(input_file, version == "hg19")
-    data_list = list(data_generator)
-    # Sort the `data_list` because `merge_rcv_accession` will call `itertools.groupby()`
-    #   which cannot group non-adjacent items with the same key into a group
-    data_list_sorted = sorted(data_list, key=lambda k: k['_id'])
-    data_merge_rcv = merge_rcv_accession(data_list_sorted)
-    return data_merge_rcv
+
+    json_record_generator = clinvar_json_record_feeder(input_file, hg19=(version == "hg19"))
+
+    # Sorting is necessary because `merge_rcv_accession` will call `itertools.groupby()`
+    #   which cannot put non-adjacent items with the same key into a group
+    json_record_list = list(json_record_generator)
+    json_record_list = sorted(json_record_list, key=lambda k: k['_id'])
+
+    merged_json_records = merge_rcv_accession(json_record_list)
+    return merged_json_records
+
 
 if __name__ == "__main__":
     from biothings.utils.mongo import get_data_folder
     data_folder = get_data_folder("clinvar")
-    load_data(data_folder=data_folder)
+    load_data(data_folder=data_folder, version="hg19")
