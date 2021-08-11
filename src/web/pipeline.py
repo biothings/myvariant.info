49317fa08688ac189895b2da506dd7a9673c40a8
Zhongchao Qian <zqian@scripps.edu>
2021-08-11 15:28:30 -0700

    added alternative parser that is measurably faster

--- src/web/pipeline.py
+++ src/web/pipeline.py
@@ -33,8 +33,64 @@ INTERVAL_PATTERN = re.compile(
     flags=re.ASCII | re.IGNORECASE | re.VERBOSE
 )
 
+INTERVAL_PATTERN_INT_ONLY = re.compile(
+    r"""
+    chr  # string literal chr   
+        (?P<chr>[1-9]|1[0-9]|2[0-2]|X|Y|MT)  # chromasomes 1-22, X, Y, and MT
+        :  # literal colon sign
+        (
+            # captures an interval
+            (?P<gstart>[\d,]+)-(?P<gend>[\d,]+)  # range, we only allow comma as sep.
+            |  # or one position
+            (?P<gpos>[\d,]+)
+        )
+    (
+        \s+AND\s+  # take a hitch on the regex engine and prepare the post_query
+        (?P<post_query_string>\S.+)  # match a non-whitespace followed by anything
+    )?
+    """,
+    flags=re.ASCII | re.IGNORECASE | re.VERBOSE
+)
+
 
 class MVQueryBuilder(ESQueryBuilder):
+    @staticmethod
+    def _parse_interval_query_v2(q: str):
+        # don't even bother when we don't see chr
+        # even with improved regex, this is a few times faster
+        start_pos = q.find('chr')
+        if start_pos < 0:
+            return None
+        pre_query = q[:start_pos].strip()
+        if pre_query != '':
+            # pre_query non empty and does not end in AND\s+
+            if not q[start_pos - 1].isspace():
+                return None
+            if pre_query[-3:].upper() != 'AND':
+                return None
+            pre_query = pre_query[:-3]  # strip the AND
+        else:
+            pre_query = None
+        m = re.match(INTERVAL_PATTERN_INT_ONLY, q[start_pos:])
+        if not m:
+            return None
+        md = m.groupdict()
+        r = {}
+        # copy chr
+        r['chr'] = md['chr']
+        # copy start/end
+        if md['gpos']:
+            r['gstart'] = r['gend'] = md['gpos']
+        else:
+            r['gstart'] = md['gstart']
+            r['gend'] = md['gend']
+        r['query'] = ' AND '.join(filter(
+                       None, (
+                           pre_query,
+                           md['post_query_string'],
+                       )))
+        return r
+
     @staticmethod
     def _parse_interval_query(q: str) -> Optional[Dict[str, str]]:
         """
