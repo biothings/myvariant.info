8af9c82e124d13695ea9722227aa840362b18187
Zhongchao Qian <zqian@scripps.edu>
2021-08-17 10:07:05 -0700

    wip

--- src/web/pipeline.py
+++ src/web/pipeline.py
@@ -5,34 +5,6 @@ from elasticsearch_dsl import Search
 from biothings.web.query import ESQueryBuilder, AsyncESQueryBackend
 
 
-INTERVAL_PATTERN = re.compile(
-    r"""
-    (?P<pre_query>
-        (?P<pre_query_string>.+)  # query string before the interval
-        \s+AND\s+  # whitespace and string literal AND and whitespace
-    )?  # pre_query is optional
-    \s*  # zero or more whitespace, if there is a pre_query, they get consumed by that one
-    (?P<interval>
-        chr  # string literal chr   
-        (?P<chr>[1-9]|1[0-9]|2[0-2]|X|Y|MT)  # chromasomes 1-22, X, Y, and MT
-        :  # literal colon sign
-        (
-            # captures an interval
-            (?P<gstart>[\d,]+)-(?P<gend>[\d,]+)  # range, we only allow comma as sep.
-            |  # or one position
-            (?P<gpos>[\d,]+)
-        )
-    )
-    \s*? # zero or more whitespace, non-greedy
-         #  so that post query can consume whitespaces, while extra spaces are also fine
-    (?P<post_query>
-        \s+AND\s+  # when post_query exists, at least one whitespace is consumed
-        (?P<post_query_string>.+)  # query string after the interval
-    )?  # optional
-    """,
-    flags=re.ASCII | re.IGNORECASE | re.VERBOSE
-)
-
 INTERVAL_PATTERN_INT_ONLY = re.compile(
     r"""
     chr  # string literal chr   
@@ -55,7 +27,7 @@ INTERVAL_PATTERN_INT_ONLY = re.compile(
 
 class MVQueryBuilder(ESQueryBuilder):
     @staticmethod
-    def _parse_interval_query_v2(q: str):
+    def _parse_interval_query(q: str):
         # don't even bother when we don't see chr
         # even with improved regex, this is a few times faster
         start_pos = q.find('chr')
@@ -90,53 +62,6 @@ class MVQueryBuilder(ESQueryBuilder):
                            md['post_query_string'],
                        )))
         return r
-
-    @staticmethod
-    def _parse_interval_query(q: str) -> Optional[Dict[str, str]]:
-        """
-        Parse query string and construct interval query parameters
-
-        Args:
-            q: query string
-
-        Returns:
-            None or dictionary with following key/values populated:
-              query - elasticsearch string query, may be empty string
-              chr - chromosome, can be numbers 1-22, X, Y, or MT
-              gstart - starting point of genomic range
-              gend - ending point of genomic range
-            gstart and gend may contain thousands separator and such separators
-             may be in the wrong place
-        """
-
-        # don't even bother when we don't see chr
-        if q.find('chr') < 0:
-            return None
-        match = re.search(INTERVAL_PATTERN, q)
-        if match:
-            md = match.groupdict()
-            r = {}
-            # copy chr
-            r['chr'] = md['chr']
-            # copy start/end
-            if md['gpos']:
-                r['gstart'] = r['gend'] = md['gpos']
-            else:
-                r['gstart'] = md['gstart']
-                r['gend'] = md['gend']
-            # construct query
-            r['query'] = ' AND '.join(
-                filter(lambda q: q != '',
-                       map(str.strip, filter(
-                           None, (
-                               md['pre_query_string'],
-                               md['post_query_string'],
-                           )
-                       )))
-            )
-            return r
-        else:
-            return None
 
     def default_string_query(self, q, options):
 
