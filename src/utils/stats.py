9da9f7c3c830ea9cdd2e6d705c39486a2bb9761e
Yao Yao <yaoy.cs@gmail.com>
2023-08-23 17:21:37 -0700

    simplify the stats updating API using ESIndexer

--- src/utils/stats.py
+++ src/utils/stats.py
@@ -2,22 +2,15 @@ from biothings.utils.es import ESIndexer
 
 
 def update_stats(idxer: ESIndexer, assembly):
-    stats = compute_stats(idxer, assembly)
+    # compute stats
+    stats = dict()
+    stats["total"] = idxer.count()
+    for k in [assembly, "observed", "vcf"]:
+        q = {"query": {"exists": {"field": k}}}
+        stats[k] = idxer._es.count(index=idxer._index, doc_type=idxer._doc_type, body=q)["count"]
+
     # now update _meta.stats
     m = idxer.get_mapping_meta()
     m["_meta"].get("stats", {}).update(stats)
     idxer.update_mapping_meta(m)
     return m["_meta"]["stats"]
-
-
-def compute_stats(idxer: ESIndexer, assembly):
-    stats = dict()
-    stats["total"] = idxer.count()
-    for k in [assembly, "observed", "vcf"]:
-        stats[k] = count_field(idxer, k)
-    return stats
-
-
-def count_field(idxer: ESIndexer, field):
-    q = {"query": {"exists": {"field": field}}}
-    return idxer._es.count(index=idxer._index, doc_type=idxer._doc_type, body=q)["count"]
