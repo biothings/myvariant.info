6fb1c71f5e25406d598743c94647c0cbc070326c
Everaldo <everaldorodrigo@gmail.com>
2024-10-11 14:10:02 -0700

    Parse molecular profile.

--- src/hub/dataload/sources/civic/civic_parser.py
+++ src/hub/dataload/sources/civic/civic_parser.py
@@ -25,41 +25,43 @@ def load_data(data_folder):
         logging.info(infile)
         doc = json.load(open(infile))
         if set(['error', 'status']) != set(doc.keys()):
-            [chrom, pos, ref, alt] = [doc['coordinates'][x] for x in ['chromosome', 'start', 'referenceBases', 'variantBases']]
-            variant_id = doc.pop("id")
-            new_doc = {}
-            doc['variant_id'] = variant_id
-            if chrom and ref and alt:
-                no_case1 += 1
-                # try:
-                #   new_doc['_id'] = get_hgvs_from_vcf(chrom, pos, ref, alt)
-                # except ValueError:
-                #   logging.warning("id has ref,alt, but coudn't be converted to hgvs id: {}".format(variant_id))
-                #   continue
-            # handle cases of deletions where only ref info is provided
-            elif chrom and ref and not alt:
-                no_case2 += 1
-                start = int(pos)
-                end = int(pos) + len(ref) - 1
-                if start == end:
-                    new_doc['_id'] = 'chr{0}:g.{1}del'.format(chrom, start)
+            if "coordinates" in doc:
+                [chrom, pos, ref, alt] = [doc['coordinates'][x] for x in ['chromosome', 'start', 'referenceBases', 'variantBases']]
+                variant_id = doc.pop("id")
+                new_doc = {}
+                doc['variant_id'] = variant_id
+                if chrom and ref and alt:
+                    no_case1 += 1
+                    # try:
+                    #   new_doc['_id'] = get_hgvs_from_vcf(chrom, pos, ref, alt)
+                    # except ValueError:
+                    #   logging.warning("id has ref,alt, but coudn't be converted to hgvs id: {}".format(variant_id))
+                    #   continue
+                # handle cases of deletions where only ref info is provided
+                elif chrom and ref and not alt:
+                    no_case2 += 1
+                    start = int(pos)
+                    end = int(pos) + len(ref) - 1
+                    if start == end:
+                        new_doc['_id'] = 'chr{0}:g.{1}del'.format(chrom, start)
+                    else:
+                        new_doc['_id'] = 'chr{0}:g.{1}_{2}del'.format(chrom, start, end)
+                # handle cases of insertions where only alt info is provided
+                elif chrom and alt and not ref:
+                    no_case3 += 1
+                    new_doc['_id'] = 'chr{0}:g.{1}_{2}ins{3}'.format(chrom, start, end, alt)
+                # handle cases where no ref or alt info provided,
+                # in this case, use CIVIC internal ID as the primary id for MyVariant.info, e.g. CIVIC_VARIANT:1
                 else:
-                    new_doc['_id'] = 'chr{0}:g.{1}_{2}del'.format(chrom, start, end)
-            # handle cases of insertions where only alt info is provided
-            elif chrom and alt and not ref:
-                no_case3 += 1
-                new_doc['_id'] = 'chr{0}:g.{1}_{2}ins{3}'.format(chrom, start, end, alt)
-            # handle cases where no ref or alt info provided,
-            # in this case, use CIVIC internal ID as the primary id for MyVariant.info, e.g. CIVIC_VARIANT:1
-            else:
-                no_case4 += 1
-                new_doc['_id'] = 'CIVIC_VARIANT:' + str(variant_id)
+                    no_case4 += 1
+                    new_doc['_id'] = 'CIVIC_VARIANT:' + str(variant_id)
+
             # for _evidence in doc['evidence_items']:
             # print(doc)
             for _molecularProfiles in doc['molecularProfiles']['nodes']:
                 # print(_molecularProfiles)
                 for _evidence in _molecularProfiles['evidenceItems']['edges']:
-                    print(_evidence['node'])
+                    # print(_evidence['node'])
                     if 'disease' in _evidence['node'] and 'doid' in (_evidence['node']['disease'] or {}) and _evidence['node']['disease']['doid']:
                         _evidence['node']['disease']['doid'] = 'DOID:' + _evidence['node']['disease']['doid']
                 #     if 'source' in _evidence and 'citation_id' in _evidence['source']:
