7093827644fbc4574378242e66d11393677e916c
Everaldo <everaldorodrigo@gmail.com>
2026-07-29 10:43:54 -0700

    Add api key to civic.

--- src/hub/dataload/sources/civic/graphql_gene.py
+++ src/hub/dataload/sources/civic/graphql_gene.py
@@ -1,5 +1,4 @@
-import logging
-import requests
+from hub.dataload.sources.civic.graphql_client import post_graphql
 
 
 class GraphqlGeneVariant():
@@ -113,12 +112,8 @@ class GraphqlGeneVariant():
         return query
 
     def fetch(self, api_url: str, variant_id: int):
-        try:
-            response = requests.post(
-                api_url,
-                json=self.gql(variant_id=variant_id)
-            )
-            return response.json()
-        except Exception as e:
-            logging.error(f"Error in {self.OPERATION_NAME}: {e}")
-            raise
+        return post_graphql(
+            api_url=api_url,
+            payload=self.gql(variant_id=variant_id),
+            operation_name=self.OPERATION_NAME,
+        )
